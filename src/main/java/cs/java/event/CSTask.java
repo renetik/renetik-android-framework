23019c324711b23279bb28a5cc6e1e040e3f24bb
Rene Dohan <dohan.rene@gmail.com>
2016-12-10 17:15:16 -0600

    Fix: Fixed exception cused by event runned after it was canceled in some special scenarios

--- src/main/java/cs/java/event/CSTask.java
+++ src/main/java/cs/java/event/CSTask.java
@@ -4,7 +4,8 @@ import cs.android.viewbase.CSViewController;
 import cs.java.collections.CSList;
 import cs.java.event.CSEvent.EventRegistration;
 
-import static cs.java.lang.Lang.*;
+import static cs.java.lang.Lang.is;
+import static cs.java.lang.Lang.list;
 
 public class CSTask<Argument> implements CSListener<Argument> {
 
@@ -12,6 +13,7 @@ public class CSTask<Argument> implements CSListener<Argument> {
     protected EventRegistration _registration;
     protected CSList<EventRegistration> _registrations = list();
     private CSListener _listener;
+    private CSViewController _parent;
 
     @SafeVarargs
     public CSTask(CSEvent<Argument>... events) {
@@ -20,6 +22,7 @@ public class CSTask<Argument> implements CSListener<Argument> {
 
     @SafeVarargs
     public CSTask(CSViewController parent, CSEvent<Argument>... events) {
+        this._parent = parent;
         for (CSEvent<Argument> event : events) _registrations.add(event.add(this));
         parent.onPause.add(new CSListener<Void>() {
             public void onEvent(EventRegistration r, Void arg) {
@@ -42,6 +45,8 @@ public class CSTask<Argument> implements CSListener<Argument> {
     public void onEvent(EventRegistration registration, Argument argument) {
         this._registration = registration;
         this._argument = argument;
+        if (is(_parent) && _parent.isPaused())
+            return;
         _listener.onEvent(registration, argument);
     }
 
