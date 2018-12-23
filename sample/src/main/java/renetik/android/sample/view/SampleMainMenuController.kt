fc4d507613f232cebafc4e83638c9be7d77a86e9
Rene Dohan <dohan.rene@gmail.com>
2018-12-22 20:59:34 -0600

    no message

--- sample/src/main/java/renetik/android/sample/view/SampleMainMenuController.kt
+++ sample/src/main/java/renetik/android/sample/view/SampleMainMenuController.kt
@@ -1,20 +1,19 @@
 package renetik.android.sample.view
 
 import android.view.View
-import org.jetbrains.anko.sdk21.coroutines.onClick
 import renetik.android.base.layout
 import renetik.android.controller.base.CSViewController
 import renetik.android.controller.common.CSLogController
 import renetik.android.extensions.button
 import renetik.android.sample.R
 import renetik.android.themes.CSThemeSwitcherController
+import renetik.android.view.extensions.onClick
 
 class SampleMainMenuController : CSViewController<View>(navigation, layout(R.layout.sample_menu)) {
     override fun onViewShowingFirstTime() {
+        menu("Themes").onClick { CSThemeSwitcherController(navigation).push() }
         button(R.id.SampleMenu_ButtonThemes).onClick { CSThemeSwitcherController(navigation).push() }
         button(R.id.SampleMenu_ButtonLog).onClick { CSLogController(navigation).push() }
         button(R.id.SampleMenu_ButtonList).onClick { SampleListController().push() }
     }
 }
-
-
--- sample/src/main/java/renetik/android/sample/view/SampleNavigationActivity.kt
+++ sample/src/main/java/renetik/android/sample/view/SampleNavigationActivity.kt
@@ -3,15 +3,20 @@ package renetik.android.sample.view
 import renetik.android.controller.base.CSActivity
 import renetik.android.controller.base.CSViewController
 import renetik.android.controller.common.CSNavigationController
+import renetik.android.themes.defaultThemes
+import renetik.android.themes.extensions.initializeThemes
 
 lateinit var navigation: CSNavigationController
 fun CSViewController<*>.push() = navigation.push(this)
 
 class SampleNavigationActivity : CSActivity() {
-    override fun createController() = object : CSNavigationController(this) {
-        override fun onViewShowingFirstTime() {
-            navigation = this
-            SampleMainMenuController().push()
+    override fun createController(): CSNavigationController {
+        initializeThemes(defaultThemes)
+        return object : CSNavigationController(this) {
+            override fun onViewShowingFirstTime() {
+                navigation = this
+                SampleMainMenuController().push()
+            }
         }
     }
 }
