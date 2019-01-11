3c7b17de459979230a53ff3051bcfe4e5fed644b
Rene Dohan <dohan.rene@gmail.com>
2019-01-10 20:01:40 -0600

    no message

--- sample/src/main/java/renetik/android/sample/view/request/SamplePagerController.kt
+++ sample/src/main/java/renetik/android/sample/view/request/SamplePagerController.kt
@@ -12,7 +12,7 @@ import renetik.android.framework.extensions.send
 import renetik.android.java.collections.list
 import renetik.android.material.extensions.floatingButton
 import renetik.android.sample.R
-import renetik.android.sample.model.SampleListItem
+import renetik.android.sample.model.ServerListItem
 import renetik.android.sample.model.model
 import renetik.android.sample.view.navigation
 import renetik.android.view.extensions.editText
@@ -34,7 +34,7 @@ class SamplePagerController(val title: String)
         val nameView = onAction.view.editText(R.id.SamplePagerAddItem_Name)
         val descView = onAction.view.editText(R.id.SamplePagerAddItem_Description)
         list(nameView, descView).validateNotEmpty("Cannot be empty") {
-            model.server.addSampleListItem(SampleListItem().apply {
+            model.server.addSampleListItem(ServerListItem().apply {
                 name.string = nameView.title
                 description.string = descView.title
             }).send("Posting item to server", progress = true)
