4ddeebb65188937abd1cef8675c9ec4b484f648e
Rene Dohan <dohan.rene@gmail.com>
2019-01-13 07:21:58 -0600

    no message

--- sample/src/main/java/renetik/android/sample/model/SampleApplication.kt
+++ sample/src/main/java/renetik/android/sample/model/SampleApplication.kt
@@ -2,6 +2,7 @@ package renetik.android.sample.model
 
 import renetik.android.base.CSApplication
 import renetik.android.base.application
+import renetik.android.crashlitics.CrashlyticsLogger
 import renetik.android.crashlitics.extensions.startFabricAnswers
 import renetik.android.crashlitics.extensions.startFabricCrashlytics
 import renetik.android.json.extensions.load
@@ -11,6 +12,7 @@ val model by lazy { application.store.load(SampleModel::class, MODEL_KEY) }
 
 class SampleApplication : CSApplication() {
     override val isDebugBuild = DEBUG
+    override val logger by lazy { CrashlyticsLogger() }
     override fun onCreate() {
         super.onCreate()
         startFabricCrashlytics()
