ce25c071fafbec2b7348b128fc8f789975d550d3
Rene Dohan <dohan.rene@gmail.com>
2021-04-05 18:10:16 -0500

    no message

--- library/renetik-base/src/main/java/renetik/android/java/event/CSStoreEventProperty.kt
+++ library/renetik-base/src/main/java/renetik/android/java/event/CSStoreEventProperty.kt
@@ -5,8 +5,8 @@ import renetik.android.base.getValue
 import renetik.android.java.event.CSEventPropertyFunctions.property
 
 class CSListStoreEventProperty<T>(
-        store: CSValueStoreInterface, val key: String, val values: List<T>,
-        val default: T, onApply: ((value: T) -> Unit)? = null)
+    store: CSValueStoreInterface, val key: String, val values: List<T>,
+    val default: T, onApply: ((value: T) -> Unit)? = null)
     : CSEventProperty<T>(store.getValue(key, values, default), onApply) {
 
     init {
@@ -21,26 +21,26 @@ class CSListStoreEventProperty<T>(
 }
 
 fun CSValueStoreInterface.property(
-        key: String, default: Boolean, onApply: ((value: Boolean) -> Unit)? = null
+    key: String, default: Boolean, onApply: ((value: Boolean) -> Unit)? = null
 ) = property(getBoolean(key, default), onApply)
-        .apply { onChange { save(key, it) } }
+    .apply { onChange { save(key, it) } }
 
 fun <T> CSValueStoreInterface.property(
-        key: String, values: List<T>, default: T, onApply: ((value: T) -> Unit)? = null
+    key: String, values: List<T>, default: T, onApply: ((value: T) -> Unit)? = null
 ) = CSListStoreEventProperty(this, key, values, default, onApply)
 
 fun CSValueStoreInterface.property(
-        key: String, default: Int, onApply: ((value: Int) -> Unit)? = null
+    key: String, default: Int, onApply: ((value: Int) -> Unit)? = null
 ) = property(getInt(key, default), onApply)
-        .apply { onChange { save(key, it) } }
+    .apply { onChange { save(key, it) } }
 
 fun CSValueStoreInterface.property(
-        key: String, default: Float,
-        onApply: ((value: Float) -> Unit)? = null
+    key: String, default: Float,
+    onApply: ((value: Float) -> Unit)? = null
 ) = property(getFloat(key, default), onApply)
-        .apply { onChange { save(key, it) } }
+    .apply { onChange { save(key, it) } }
 
 fun CSValueStoreInterface.property(
-        key: String, default: String, onChange: ((value: String) -> Unit)? = null
+    key: String, default: String, onChange: ((value: String) -> Unit)? = null
 ) = property(getString(key, default), onChange)
-        .apply { onChange { save(key, it) } }
+    .apply { onChange { save(key, it) } }
