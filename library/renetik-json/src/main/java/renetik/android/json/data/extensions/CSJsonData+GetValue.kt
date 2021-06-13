48ad653f41036ab68f87c5a293b9bb2401345758
Rene Dohan <dohan.rene@gmail.com>
2021-06-12 22:05:16 -0500

    no message

--- library/renetik-json/src/main/java/renetik/android/json/data/extensions/CSJsonData+GetValue.kt
+++ library/renetik-json/src/main/java/renetik/android/json/data/extensions/CSJsonData+GetValue.kt
@@ -2,6 +2,6 @@ package  renetik.android.json.data.extensions
 
 import renetik.android.json.data.CSJsonMap
 
-fun CSJsonMap.getStringValue(key: String): String = getString(key) ?: ""
-fun CSJsonMap.getIntValue(key: String): Int = getInt(key) ?: 0
-fun CSJsonMap.getBooleanValue(key: String): Boolean = getBoolean(key) ?: false
+fun CSJsonMap.getStringValue(key: String, default: String = "") = getString(key) ?: default
+fun CSJsonMap.getIntValue(key: String, default: Int = 0) = getInt(key) ?: default
+fun CSJsonMap.getBooleanValue(key: String, default: Boolean = false) = getBoolean(key) ?: default
