d0a5a32f6483606db4d43bbd4bac48b4f3ee529c
Rene Dohan <dohan.rene@gmail.com>
2021-06-17 12:22:46 -0500

    no message

---
+++ library/renetik-controller/src/main/java/renetik/android/controller/common/CSHeadsetAudioPlugDetector.kt
@@ -0,0 +1,47 @@
+package renetik.android.controller.common
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.view.View
+import renetik.android.controller.base.CSViewController
+import renetik.android.logging.CSLog.logInfo
+import renetik.android.logging.CSLog.logWarn
+
+class CSHeadsetAudioPlugDetector(
+    parent: CSViewController<*>, val onHeadsetPlugChanged: (isPlugged: Boolean) -> Unit)
+    : CSViewController<View>(parent) {
+
+    private val receiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context, intent: Intent) = onReceive(intent)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        registerReceiver(receiver, IntentFilter(Intent.ACTION_HEADSET_PLUG))
+    }
+
+    override fun onPause() {
+        super.onPause()
+        unregisterReceiver(receiver)
+    }
+
+    fun onReceive(intent: Intent) {
+        if (intent.action == Intent.ACTION_HEADSET_PLUG) {
+            logInfo("ACTION_HEADSET_PLUG isInitialStickyBroadcast",
+                    receiver.isInitialStickyBroadcast)
+            when (intent.getIntExtra("state", -1)) {
+                0 -> {
+                    logInfo("ACTION_HEADSET_PLUG isUnplugged")
+                    onHeadsetPlugChanged(false)
+                }
+                1 -> {
+                    logInfo("ACTION_HEADSET_PLUG isPlugged")
+                    onHeadsetPlugChanged(true)
+                }
+                else -> logWarn("ACTION_HEADSET_PLUG unknown")
+            }
+        }
+    }
+}
