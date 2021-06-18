f20a38d35f26144689631ffe55b28cb761337795
Rene Dohan <dohan.rene@gmail.com>
2021-06-18 14:37:33 -0500

    no message

--- library/renetik-base/src/main/java/renetik/android/primitives/Int+CSExtension.kt
+++ library/renetik-base/src/main/java/renetik/android/primitives/Int+CSExtension.kt
@@ -2,7 +2,7 @@ package renetik.android.primitives
 
 import kotlin.random.Random
 
-val Int.Companion.empty get() = Int.MAX_VALUE
+val Int.Companion.empty get() = MAX_VALUE
 fun Int.Companion.random(min: Int, max: Int): Int {
     if (min >= max) throw IllegalArgumentException("max must be greater than min")
     return Random.nextInt(max - min + 1) + min
@@ -12,3 +12,6 @@ val Int.isEmpty get() = this == Int.empty
 val Int.isSet get() = !this.isEmpty
 fun Int.max(maximumValue: Int) = if (this < maximumValue) this else maximumValue
 fun Int.min(minimumValue: Int) = if (this > minimumValue) this else minimumValue
+
+fun Int.isSetIn(bitwise: Int) = bitwise and this != 0
+fun Int.isNotIn(bitwise: Int) = !this.isSetIn(bitwise)
