d2244668eeeb6d068cf6d6bd6a375142dbb2d45a
Rene Dohan <dohan.rene@gmail.com>
2021-06-18 19:24:53 -0500

    no message

--- library/renetik-base/src/main/java/renetik/android/primitives/Boolean+CSExtension.kt
+++ library/renetik-base/src/main/java/renetik/android/primitives/Boolean+CSExtension.kt
@@ -2,15 +2,8 @@ package renetik.android.primitives
 
 val Boolean.Companion.random get() = Int.random(0, 1) == 1
 
-fun Boolean.ifIs(function: () -> Unit) {
-    if (this) function()
-}
-
-fun Boolean.ifNot(function: () -> Unit) {
-    if (!this) function()
-}
-
+fun <T> Boolean.ifTrue(function: () -> T): T? = if (this) function() else null
+fun <T> Boolean.ifFalse(function: () -> T): T? = if (!this) function() else null
 val <T : Boolean?> T.isTrue: Boolean get() = this == true
 val <T : Boolean?> T.isNotTrue: Boolean get() = this != true
-
 val <T : Boolean?> T.isFalse: Boolean get() = this == false
