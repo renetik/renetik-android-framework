692ef6c8707166b8a5f8200019754ffa7c4b3edd
Rene Dohan <dohan.rene@gmail.com>
2021-07-01 18:02:15 -0500

    no message

--- library/renetik-base/src/main/java/renetik/android/java/util/Timer+CSExtension.kt
+++ library/renetik-base/src/main/java/renetik/android/java/util/Timer+CSExtension.kt
@@ -4,14 +4,29 @@ import renetik.android.os.CSHandler.post
 import java.util.*
 
 object CSTimer {
+    private var timer = Timer()
+
     fun scheduleAtFixedRateRunOnUI(delay: Long = 0, period: Long, function: () -> Unit) =
         scheduleAtFixedRate(delay, period) { post(function) }
 
-    fun scheduleAtFixedRate(delay: Long = 0, period: Long, function: () -> Unit) = Timer().apply {
-        scheduleAtFixedRate(object : TimerTask() {
-            override fun run() {
-                function()
-            }
-        }, delay, period)
+    fun scheduleAtFixedRate(delay: Long = 0, period: Long, function: () -> Unit) =
+        timer.scheduleAtFixedRate(delay, period, function)
+
+    fun cancel() {
+        timer.cancel()
+        timer = Timer()
     }
+}
+
+fun Timer.scheduleAtFixedRateRunOnUI(delay: Long = 0, period: Long, function: () -> Unit) =
+    scheduleAtFixedRate(delay, period) { post(function) }
+
+fun Timer.scheduleAtFixedRate(delay: Long = 0, period: Long, function: () -> Unit): TimerTask {
+    val task = object : TimerTask() {
+        override fun run() {
+            function()
+        }
+    }
+    scheduleAtFixedRate(task, delay, period)
+    return task
 }
