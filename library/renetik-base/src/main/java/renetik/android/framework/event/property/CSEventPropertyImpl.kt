33dfcd7e0eaf429953a5e0a69de024cce674b0af
Rene Dohan <dohan.rene@gmail.com>
2021-08-13 11:47:49 -0500

    no message

--- library/renetik-base/src/main/java/renetik/android/framework/event/property/CSEventPropertyImpl.kt
+++ library/renetik-base/src/main/java/renetik/android/framework/event/property/CSEventPropertyImpl.kt
@@ -18,7 +18,7 @@ class CSEventPropertyImpl<T>(value: T, onApply: ((value: T) -> Unit)? = null) :
 }
 
 abstract class CSStoreEventPropertyBase<T>(
-    var store: CSStoreInterface,val key: String, onApply: ((value: T) -> Unit)? = null) :
+    var store: CSStoreInterface, val key: String, onApply: ((value: T) -> Unit)? = null) :
     CSEventPropertyBase<T>(onApply) {
 
     fun store(store: CSStoreInterface) = apply {
@@ -52,7 +52,7 @@ abstract class CSStoreEventPropertyBase<T>(
 abstract class CSEventPropertyBase<T>(
     protected val onApply: ((value: T) -> Unit)? = null) : CSEventProperty<T> {
     protected val eventBeforeChange = event<T>()
-     val eventChange = event<T>() // Temporary public for debugging
+    protected val eventChange = event<T>()
     protected abstract var _value: T
 
     override var value: T
@@ -61,6 +61,7 @@ abstract class CSEventPropertyBase<T>(
 
     override fun onBeforeChange(value: (T) -> Unit) = eventBeforeChange.listen(value)
     override fun onChange(value: (T) -> Unit) = eventChange.listen(value)
+
     override fun apply() = apply {
         val value = _value
         onApply?.invoke(value)
