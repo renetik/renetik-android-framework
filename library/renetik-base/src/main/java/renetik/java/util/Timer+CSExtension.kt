51fce9a4e2a10f1e16b2fc81042ffa28f31c4df6
Rene Dohan <dohan.rene@gmail.com>
2021-09-20 15:56:24 -0500

    no message

--- library/renetik-base/src/main/java/renetik/java/util/Timer+CSExtension.kt
+++ library/renetik-base/src/main/java/renetik/java/util/Timer+CSExtension.kt
@@ -3,31 +3,32 @@ package renetik.java.util
 import renetik.android.framework.common.catchAllError
 import renetik.android.framework.util.CSHandler.post
 import java.util.*
-import java.util.concurrent.Executors
 import java.util.concurrent.Executors.newSingleThreadScheduledExecutor
-import java.util.concurrent.TimeUnit
+import java.util.concurrent.TimeUnit.MILLISECONDS
+import java.util.concurrent.TimeUnit.NANOSECONDS
 import kotlin.concurrent.timerTask
 
 object CSTimer {
-    val executor = newSingleThreadScheduledExecutor()
 
+    val executor = newSingleThreadScheduledExecutor()
 //    private val timer = Timer()
 
     fun scheduleAtFixedRateRunOnUI(delay: Long = 0, period: Long, function: () -> Unit) =
         scheduleAtFixedRate(delay, period) { post(function) }
 
     fun scheduleAtFixedRate(delay: Long = 0, period: Long, function: () -> Unit) =
-        executor.scheduleAtFixedRate(function, delay, period, TimeUnit.MILLISECONDS)
+        executor.scheduleAtFixedRate({ catchAllError { function() } },
+            delay, period, MILLISECONDS)
 //    timer.scheduleAtFixedRate(delay, period, function)
 
     fun scheduleRunOnUI(delay: Long = 0, function: () -> Unit) =
         schedule(delay) { post(function) }
 
     fun schedule(delay: Long = 0, function: () -> Unit) =
-        executor.schedule(function, delay, TimeUnit.MILLISECONDS)
+        executor.schedule({ catchAllError { function() } }, delay, MILLISECONDS)
 
     fun scheduleNano(delay: Long = 0, function: () -> Unit) =
-        executor.schedule(function, delay, TimeUnit.NANOSECONDS)
+        executor.schedule({ catchAllError { function() } }, delay, NANOSECONDS)
 //        executor.scheduleAtFixedRate(function, period, delay, TimeUnit.MILLISECONDS)
 //        timer.schedule(delay, function)
 }
