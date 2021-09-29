4d6ac30598fe5917b8233e0a839e69a53de0cc38
Rene Dohan <dohan.rene@gmail.com>
2021-09-29 16:24:02 -0500

    no message

--- library/renetik-base/src/main/java/renetik/android/widget/NumberPicker+.kt
+++ library/renetik-base/src/main/java/renetik/android/widget/NumberPicker+.kt
@@ -16,6 +16,9 @@ import android.widget.NumberPicker.FOCUS_BEFORE_DESCENDANTS
 import android.widget.NumberPicker.FOCUS_BLOCK_DESCENDANTS
 import androidx.annotation.ColorInt
 import renetik.android.framework.Func
+import renetik.android.framework.event.CSEventRegistration
+import renetik.android.framework.event.pause
+import renetik.android.framework.event.property.CSEventProperty
 import renetik.kotlin.asStringArray
 import renetik.kotlin.collections.hasItems
 import renetik.kotlin.collections.index
@@ -44,7 +47,7 @@ fun <Row : Any> NumberPicker.loadData(data: List<Row>, selectedIndex: Int = 0) =
 
 val NumberPicker.index: Int get() = value
 
-fun NumberPicker.onSelected(function: Func) = apply {
+fun NumberPicker.onValueChange(function: Func) = apply {
     setOnValueChangedListener { _, _, _ -> function() }
 }
 
@@ -55,6 +58,24 @@ fun NumberPicker.circulate(circulate: Boolean) = apply {
 fun NumberPicker.disableTextEditing(disable: Boolean) = apply {
     descendantFocusability = if (disable) FOCUS_BLOCK_DESCENDANTS else FOCUS_BEFORE_DESCENDANTS
 }
+
+fun NumberPicker.min(value: Int) = apply { minValue = value }
+
+fun NumberPicker.max(value: Int) = apply { maxValue = value }
+
+@JvmName("valuePropertyInt")
+fun NumberPicker.value(property: CSEventProperty<Int>): CSEventRegistration {
+    val registration = value(property) { it }
+    onValueChange { registration.pause().use { property.value = value } }
+    return registration
+}
+
+fun <T> NumberPicker.value(property: CSEventProperty<T>, function: (T) -> Int)
+        : CSEventRegistration {
+    value = function(property.value)
+    return property.onChange { value = function(property.value) }
+}
+
 
 // setSelectionDividerHeight requires api 29
 fun NumberPicker.selectionDividerHeight(height: Int) =
