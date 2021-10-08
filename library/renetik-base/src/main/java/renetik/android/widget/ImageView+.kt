d8dbd7ae1976374526919a835299fac509681b33
Rene Dohan <dohan.rene@gmail.com>
2021-10-08 12:46:42 -0500

    no message

--- library/renetik-base/src/main/java/renetik/android/widget/ImageView+.kt
+++ library/renetik-base/src/main/java/renetik/android/widget/ImageView+.kt
@@ -5,11 +5,8 @@ import android.graphics.BitmapFactory.decodeFile
 import android.widget.ImageView
 import androidx.annotation.ColorInt
 import androidx.annotation.DrawableRes
-import renetik.android.framework.common.catchAllErrorReturnNull
 import renetik.android.framework.event.CSEventRegistration
 import renetik.android.framework.event.property.CSEventProperty
-import renetik.android.framework.task.CSBackground.background
-import renetik.kotlin.later
 import java.io.File
 
 fun <T : ImageView> T.iconTint(@ColorInt color: Int) =
@@ -19,10 +16,17 @@ fun <T : ImageView> T.image(@DrawableRes resourceId: Int) =
     apply { setImageResource(resourceId) }
 
 fun <T : ImageView> T.image(file: File) = apply {
-    background {
-        catchAllErrorReturnNull { decodeFile(file.absolutePath) }
-            ?.let { bitmap -> later { setImageBitmap(bitmap) } }
-    }
+//    invisible()
+//    posting on background slows displaying significantly for unknown reason
+//    background {
+//        catchAllErrorReturnNull { decodeFile(file.absolutePath) }?.let { bitmap ->
+//            post {
+//                setImageBitmap(bitmap)
+//                visible()
+//            }
+//        }
+//    }
+    setImageBitmap(decodeFile(file.absolutePath))
 }
 
 fun <T> ImageView.image(property: CSEventProperty<T>,
