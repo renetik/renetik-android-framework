d08750f0672cd1badb89836234914b262e29ee8d
Rene Dohan <dohan.rene@gmail.com>
2021-12-15 18:20:07 -0500

    no message

--- library/renetik-base/src/main/java/renetik/android/framework/store/property/value/CSValueStoreEventProperty.kt
+++ library/renetik-base/src/main/java/renetik/android/framework/store/property/value/CSValueStoreEventProperty.kt
@@ -7,18 +7,24 @@ import renetik.android.framework.store.CSStoreInterface
 import renetik.android.framework.store.property.CSStoreEventProperty
 
 abstract class CSValueStoreEventProperty<T>(
-    final override val store: CSStoreInterface,
-    final override val key: String,
-    val listenStoreChanged: Boolean = false,
-    onChange: ((value: T) -> Unit)? = null)
+        final override val store: CSStoreInterface,
+        final override val key: String,
+        val listenStoreChanged: Boolean = false,
+        onChange: ((value: T) -> Unit)? = null)
     : CSEventPropertyBase<T>(onChange), CSStoreEventProperty<T> {
 
     abstract val defaultValue: T
     protected abstract var _value: T
     abstract fun get(store: CSStoreInterface): T?
+    var isLoaded = false
 
-    fun load(store: CSStoreInterface): T = get(store) ?: defaultValue
-    fun load(): T = load(store)
+    fun load(): T {
+        val value = get(store)
+        return if (value != null) {
+            isLoaded = true
+            value
+        } else defaultValue
+    }
 
     private val storeEventChangedRegistration = if (listenStoreChanged) store.eventChanged.listen {
         val newValue = load()
@@ -39,8 +45,9 @@ abstract class CSValueStoreEventProperty<T>(
         set(value) = value(value)
 
     override fun value(newValue: T, fire: Boolean) {
-        if (_value == newValue) return
+        if (_value == newValue && isLoaded) return
         _value = newValue
+        isLoaded = true
         storeEventChangedRegistration?.pause()?.use {
             set(store, newValue)
             onApply?.invoke(newValue)
