ceb60b018501dddd20cb722f87ee657a146c57dc
Rene Dohan <dohan.rene@gmail.com>
2022-01-11 14:02:10 -0500

    no message

--- library/renetik-base/src/main/java/renetik/android/framework/view/CSImageView.kt
+++ library/renetik-base/src/main/java/renetik/android/framework/view/CSImageView.kt
@@ -3,14 +3,10 @@ package renetik.android.framework.view
 import android.content.Context
 import android.util.AttributeSet
 import android.view.MotionEvent
-import android.view.MotionEvent.*
 import android.view.View.MeasureSpec.EXACTLY
 import android.view.View.MeasureSpec.makeMeasureSpec
 import android.widget.ImageView
 import renetik.android.R
-import renetik.android.framework.event.CSEventRegistration
-import renetik.android.framework.event.pause
-import renetik.android.framework.event.property.CSEventProperty
 
 class CSImageView @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null,
@@ -18,6 +14,7 @@ class CSImageView @JvmOverloads constructor(
 ) : ImageView(context, attrs, defStyleAttr, defStyleRes), CSHasTouchEvent {
 
     override val self = this
+    private val _maxWidth: Int
     private val _maxHeight: Int
     override var onTouchEvent: ((event: MotionEvent) -> Boolean)? = null
 
@@ -26,6 +23,7 @@ class CSImageView @JvmOverloads constructor(
             context.theme.obtainStyledAttributes(attrs, R.styleable.CSLayout, 0, 0)
         try {
             clipToOutline = attributes.getBoolean(R.styleable.CSLayout_clipToOutline, false)
+            _maxWidth = attributes.getDimensionPixelSize(R.styleable.CSLayout_maxWidth, -1)
             _maxHeight = attributes.getDimensionPixelSize(R.styleable.CSLayout_maxHeight, -1)
         } finally {
             attributes.recycle()
@@ -34,7 +32,9 @@ class CSImageView @JvmOverloads constructor(
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec)
-        if (_maxHeight != -1 && measuredHeight > _maxHeight)
+        if (_maxWidth != -1 && measuredWidth > _maxWidth)
+            super.onMeasure(makeMeasureSpec(_maxWidth, EXACTLY), heightMeasureSpec)
+        else if (_maxHeight != -1 && measuredHeight > _maxHeight)
             super.onMeasure(widthMeasureSpec, makeMeasureSpec(_maxHeight, EXACTLY))
     }
 
