b850a8adb544a02dce5f4d489c267f3eb3b1d18b
Rene Dohan <dohan.rene@gmail.com>
2022-01-15 01:06:43 -0500

    no message

--- library/renetik-base/src/main/java/renetik/java/util/concurrent/ScheduledExecutorService+.kt
+++ library/renetik-base/src/main/java/renetik/java/util/concurrent/ScheduledExecutorService+.kt
@@ -8,37 +8,34 @@ import java.util.concurrent.ScheduledExecutorService
 import java.util.concurrent.TimeUnit.MILLISECONDS
 import java.util.concurrent.TimeUnit.NANOSECONDS
 
-fun ScheduledExecutorService.background(delay: Long = 0,
-                                        @WorkerThread function: () -> Unit) =
+inline fun ScheduledExecutorService.background(
+    delay: Long = 0, @WorkerThread crossinline function: () -> Unit) =
     schedule({ catchAllError { function() } }, delay, MILLISECONDS)
 
-fun ScheduledExecutorService.backgroundNano(delay: Long = 0,
-                                            @WorkerThread function: () -> Unit) =
+inline fun ScheduledExecutorService.backgroundNano(
+    delay: Long = 0, @WorkerThread crossinline function: () -> Unit) =
     schedule({ catchAllError { function() } }, delay, NANOSECONDS)
 
-fun ScheduledExecutorService.backgroundRunOnUi(delay: Long = 0,
-                                               @UiThread function: () -> Unit) =
+fun ScheduledExecutorService.backgroundRunOnUi(
+    delay: Long = 0, @UiThread function: () -> Unit) =
     background(delay) { postOnMain(function) }
 
-fun ScheduledExecutorService.backgroundRepeat(delay: Long = 0,
-                                              period: Long,
-                                              @WorkerThread function: () -> Unit) =
+inline fun ScheduledExecutorService.backgroundRepeat(
+    delay: Long = 0, period: Long, @WorkerThread crossinline function: () -> Unit) =
     scheduleAtFixedRate({ catchAllError { function() } }, delay, period, MILLISECONDS)
 
-fun ScheduledExecutorService.backgroundRepeatNano(delay: Long = 0,
-                                                  period: Long,
-                                                  @WorkerThread function: () -> Unit) =
+inline fun ScheduledExecutorService.backgroundRepeatNano(
+    delay: Long = 0, period: Long, @WorkerThread crossinline function: () -> Unit) =
     scheduleAtFixedRate({ catchAllError { function() } }, delay, period, NANOSECONDS)
 
-fun ScheduledExecutorService.backgroundRepeatRunOnUI(delay: Long = 0,
-                                                     period: Long,
-                                                     @UiThread function: () -> Unit) =
+fun ScheduledExecutorService.backgroundRepeatRunOnUI(
+    delay: Long = 0, period: Long, @UiThread function: () -> Unit) =
     backgroundRepeat(delay, period) { postOnMain(function) }
 
-fun ScheduledExecutorService.backgroundRepeat(period: Long,
-                                              @WorkerThread function: () -> Unit) =
+inline fun ScheduledExecutorService.backgroundRepeat(
+    period: Long, @WorkerThread crossinline function: () -> Unit) =
     backgroundRepeat(delay = period, period = period, function = function)
 
-fun ScheduledExecutorService.backgroundRepeatNano(period: Long,
-                                                  @WorkerThread function: () -> Unit) =
+inline fun ScheduledExecutorService.backgroundRepeatNano(
+    period: Long, @WorkerThread crossinline function: () -> Unit) =
     backgroundRepeatNano(delay = period, period = period, function = function)
