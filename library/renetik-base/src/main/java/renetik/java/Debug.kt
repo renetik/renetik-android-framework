ad6c5c72ce310272a97b8670d0b6c213f59688d4
Rene Dohan <dohan.rene@gmail.com>
2022-01-26 14:49:44 -0500

    no message

--- library/renetik-base/src/main/java/renetik/java/Debug.kt
+++ library/renetik-base/src/main/java/renetik/java/Debug.kt
@@ -2,11 +2,11 @@ package renetik.java
 
 import renetik.android.framework.lang.CSTimeConstants.MilliToNanoSecondMultiplier
 import renetik.android.framework.lang.CSTimeConstants.Second
-import renetik.android.framework.logging.CSLog.info
+import renetik.android.framework.logging.CSLog.debug
 
 fun measureTimeElapsed(function: () -> Unit) {
     val time = System.nanoTime()
     function()
     val duration = System.nanoTime() - time
-    info("Duration ${duration / MilliToNanoSecondMultiplier / Second} Seconds")
+    debug("Duration ${duration / MilliToNanoSecondMultiplier / Second} Seconds")
 }
