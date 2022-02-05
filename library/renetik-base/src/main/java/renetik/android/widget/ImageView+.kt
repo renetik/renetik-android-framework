38ce4fdbc71361c6cfd5ad6b46525d4d7a147ffd
Rene Dohan <dohan.rene@gmail.com>
2022-02-05 12:47:21 -0500

    Update ImageView+.kt

--- library/renetik-base/src/main/java/renetik/android/widget/ImageView+.kt
+++ library/renetik-base/src/main/java/renetik/android/widget/ImageView+.kt
@@ -2,18 +2,11 @@ package renetik.android.widget
 
 //import coil.load
 import android.content.res.ColorStateList
-import android.graphics.drawable.PictureDrawable
 import android.widget.ImageView
 import androidx.annotation.ColorInt
 import androidx.annotation.DrawableRes
 import com.bumptech.glide.Glide
-import com.bumptech.glide.load.DataSource
-import com.bumptech.glide.load.engine.GlideException
-import com.bumptech.glide.request.RequestListener
-import com.bumptech.glide.request.target.ImageViewTarget
-import com.bumptech.glide.request.target.Target
 import com.bumptech.glide.signature.ObjectKey
-import com.github.twocoffeesoneteam.glidetovectoryou.GlideToVectorYouListener
 import renetik.android.framework.event.CSEventRegistration
 import renetik.android.framework.event.property.CSEventProperty
 import java.io.File
