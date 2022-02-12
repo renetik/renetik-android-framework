129038b3ddf0641ba460bc8558bfd8beebd7375b
Rene Dohan <dohan.rene@gmail.com>
2022-02-12 00:54:28 -0500

    Update CSHasDestroy.kt

--- library/renetik-base/src/main/java/renetik/android/framework/event/CSHasDestroy.kt
+++ library/renetik-base/src/main/java/renetik/android/framework/event/CSHasDestroy.kt
@@ -3,4 +3,8 @@ package renetik.android.framework.event
 interface CSHasDestroy {
     val eventDestroy: CSEvent<Unit>
     fun onDestroy()
+
+    companion object {
+        fun CSHasDestroy.onDestroy(listener: () -> Unit) = eventDestroy.listenOnce { listener() }
+    }
 }
