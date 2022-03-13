50b729a035025482399b42da7294e00bf8c1f100
Rene Dohan <dohan.rene@gmail.com>
2022-03-12 22:46:54 -0500

    Update CSEventOwner+.kt

--- library/renetik-base/src/main/java/renetik/android/framework/event/CSEventOwner+.kt
+++ library/renetik-base/src/main/java/renetik/android/framework/event/CSEventOwner+.kt
@@ -3,8 +3,9 @@ package renetik.android.framework.event
 fun CSEventOwner.register(registration: CSEventRegistration) =
     registration.also { eventRegistrations.add(it) }
 
-fun CSEventOwner.register(key: Any, registration: CSEventRegistration) =
-    registration.also { eventRegistrations.add(key, it) }
+fun CSEventOwner.register(key: Any, registration: CSEventRegistration) {
+    eventRegistrations.add(key, registration)
+}
 
 fun CSEventOwner.cancel(registration: CSEventRegistration) =
     registration.also { eventRegistrations.cancel(it) }
