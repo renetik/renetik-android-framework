a68d9aa93abe711b2ee7ce6945ebe29538cd1707
Rene Dohan <dohan.rene@gmail.com>
2022-03-17 22:38:35 -0500

    Update ImageView+.kt

--- library/renetik-base/src/main/java/renetik/android/widget/ImageView+.kt
+++ library/renetik-base/src/main/java/renetik/android/widget/ImageView+.kt
@@ -2,6 +2,7 @@ package renetik.android.widget
 
 //import coil.load
 import android.content.res.ColorStateList
+import android.net.Uri
 import android.widget.ImageView
 import androidx.annotation.ColorInt
 import androidx.annotation.DrawableRes
@@ -24,19 +25,22 @@ fun <T : ImageView> T.image(
     } ?: run { setImageDrawable(null) }
 }
 
-fun <T : ImageView> T.image(file: File) = apply {
+fun <T : ImageView> T.image(file: File, useAndroidSdk: Boolean = false) = apply {
 //    load(file)
-//    setImageBitmap(decodeFile(file.absolutePath))
-
+    if (useAndroidSdk)
+        setImageURI(Uri.fromFile(file));
+//        setImageBitmap(decodeFile(file.absolutePath))
+    else
     //Cache invalidation https://muyangmin.github.io/glide/doc/caching.html#cache-invalidation
-    Glide.with(context).load(file)
-        .signature(ObjectKey(file.lastModified())).into(this)
+        Glide.with(context).load(file)
+            .signature(ObjectKey(file.lastModified())).into(this)
 }
 
 fun <T> ImageView.image(property: CSEventProperty<T>,
+                        useAndroidSdk: Boolean = false,
                         valueToImage: (T) -> File
 ): CSRegistration {
-    fun updateImage() = image(valueToImage(property.value))
+    fun updateImage() = image(valueToImage(property.value), useAndroidSdk)
     updateImage()
     return property.onChange { updateImage() }
 }
