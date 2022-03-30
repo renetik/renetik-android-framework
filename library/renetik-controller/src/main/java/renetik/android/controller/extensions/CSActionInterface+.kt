21170b619f77ffe2ea60b7264b597d8ffe7ba20f
Rene Dohan <dohan.rene@gmail.com>
2022-03-30 15:06:36 -0500

    Update CSActionInterface+.kt

--- library/renetik-controller/src/main/java/renetik/android/controller/extensions/CSActionInterface+.kt
+++ library/renetik-controller/src/main/java/renetik/android/controller/extensions/CSActionInterface+.kt
@@ -11,27 +11,27 @@ import renetik.android.framework.lang.property.isTrue
 import renetik.android.framework.lang.property.setFalse
 import renetik.android.view.onClick
 
-fun CSActionInterface.dialog(
-    dialog: CSNavigationDialog<ViewGroup>,
-    animation: DialogAnimation = DialogAnimation.Slide): CSRegistration {
-    dialog.onDismiss { setFalse() }
-    if (isTrue) dialog.show(DialogAnimation.None)
-    return onTrue { dialog.show(animation) }
-}
+//fun CSActionInterface.dialog(
+//    dialog: CSNavigationDialog<ViewGroup>,
+//    animation: DialogAnimation = DialogAnimation.Slide): CSRegistration {
+//    dialog.onDismiss { setFalse() }
+//    if (isTrue) dialog.show(DialogAnimation.None)
+//    return onTrue { dialog.show(animation) }
+//}
 
-fun CSActionInterface.dialog(
-    function: () -> CSNavigationDialog<*>): CSRegistration {
-    fun show() {
-        val dialog = function()
-        dialog.onDismiss { setFalse() }
-    }
-    if (isTrue) show()
-    return onTrue { show() }
-}
+//fun CSActionInterface.dialog(
+//    function: () -> CSNavigationDialog<*>): CSRegistration {
+//    fun show() {
+//        val dialog = function()
+//        dialog.onDismiss { setFalse() }
+//    }
+//    if (isTrue) show()
+//    return onTrue { show() }
+//}
 
-fun View.actionDialog(
-    action: CSActionInterface,
-    function: () -> CSNavigationDialog<*>): CSRegistration {
-    onClick(action)
-    return action.dialog(function)
-}
+//fun View.actionDialog(
+//    action: CSActionInterface,
+//    function: () -> CSNavigationDialog<*>): CSRegistration {
+//    onClick(action)
+//    return action.dialog(function)
+//}
