1c1d83c29e5f97d9aaa59df6e7a92a0acbd86c66
Rene Dohan <dohan.rene@gmail.com>
2022-04-01 15:47:08 -0500

    Update Context+Service.kt

--- library/renetik-base/src/main/java/renetik/android/content/Context+Service.kt
+++ library/renetik-base/src/main/java/renetik/android/content/Context+Service.kt
@@ -11,6 +11,13 @@ import androidx.annotation.RequiresApi
 
 val Context.bluetooth: BluetoothManager get() = getSystemService(BLUETOOTH_SERVICE) as BluetoothManager
 val Context.input: InputMethodManager get() = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+
+val Context.isMidiSupported: Boolean
+    @RequiresApi(Build.VERSION_CODES.M) get() = getSystemService(MIDI_SERVICE) != null
+
+@RequiresApi(Build.VERSION_CODES.M)
+fun <T> Context.isMidiSupported(function: () -> T): T? = if (isMidiSupported) function() else null
+
 val Context.midi: MidiManager
     @RequiresApi(Build.VERSION_CODES.M) get() = getSystemService(MIDI_SERVICE) as MidiManager
 
