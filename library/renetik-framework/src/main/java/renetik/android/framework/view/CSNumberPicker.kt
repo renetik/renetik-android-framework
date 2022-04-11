90057f7f3b2a4749d16bcb17b1356c04fec6034f
Rene Dohan <dohan.rene@gmail.com>
2022-04-11 00:34:36 -0500

    Update CSNumberPicker.kt

--- library/renetik-framework/src/main/java/renetik/android/framework/view/CSNumberPicker.kt
+++ library/renetik-framework/src/main/java/renetik/android/framework/view/CSNumberPicker.kt
@@ -3,9 +3,9 @@ package renetik.android.framework.view
 import android.content.Context
 import android.util.AttributeSet
 import com.shawnlin.numberpicker.NumberPicker
+import com.shawnlin.numberpicker.NumberPicker.OnScrollListener.SCROLL_STATE_IDLE
 import renetik.android.framework.event.CSEvent.Companion.event
 import renetik.android.framework.event.property.CSEventPropertyFunctions.property
-import renetik.android.framework.logging.CSLog.info
 
 class CSNumberPicker @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
@@ -15,13 +15,16 @@ class CSNumberPicker @JvmOverloads constructor(
         value = it
     }
     val eventOnScroll = event<Int>()
+    private var isScrolling: Boolean = false
 
     init {
         setOnScrollListener { _, scrollState ->
-//            isScrolling = scrollState != SCROLL_STATE_IDLE
-//            if (!isScrolling) valueProperty.value(value)
+            isScrolling = scrollState != SCROLL_STATE_IDLE
+            if (!isScrolling) valueProperty.value(value)
             eventOnScroll.fire(scrollState)
         }
-        setOnValueChangedListener { _, _, newValue -> valueProperty.value(newValue) }
+        setOnValueChangedListener { _, _, newValue ->
+            if (!isScrolling) valueProperty.value(newValue)
+        }
     }
 }
