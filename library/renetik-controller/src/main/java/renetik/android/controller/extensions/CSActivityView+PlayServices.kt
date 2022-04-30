a83bed65d23fcf40919f9ba72c3463be58b8989c
Rene Dohan <dohan.rene@gmail.com>
2022-04-30 14:53:46 -0500

    ...

--- library/renetik-controller/src/main/java/renetik/android/controller/extensions/CSActivityView+PlayServices.kt
+++ library/renetik-controller/src/main/java/renetik/android/controller/extensions/CSActivityView+PlayServices.kt
@@ -1,21 +1,21 @@
-package renetik.android.controller.extensions
-
-import com.google.android.gms.common.ConnectionResult
-import com.google.android.gms.common.GoogleApiAvailability
-import renetik.android.controller.base.CSActivityView
-import renetik.android.framework.lang.CSUserAction
-
-private const val RESOLUTION_REQUEST = 9000
-
-fun <T : CSActivityView<*>> T.checkPlayServices() {
-    val apiAvailability = GoogleApiAvailability.getInstance()
-    val result = apiAvailability.isGooglePlayServicesAvailable(this)
-    if (result != ConnectionResult.SUCCESS) {
-        if (apiAvailability.isUserResolvableError(result))
-            apiAvailability.getErrorDialog(activity(), result, RESOLUTION_REQUEST)?.show()
-                ?: snackBarError("Google Play Services missing application cannot continue",
-                    CSUserAction("Finish") { activity().finish() })
-        else snackBarError("Google Play Services missing application cannot continue",
-            CSUserAction("Finish") { activity().finish() })
-    }
-}
+//package renetik.android.controller.extensions
+//
+//import com.google.android.gms.common.ConnectionResult
+//import com.google.android.gms.common.GoogleApiAvailability
+//import renetik.android.controller.base.CSActivityView
+//import renetik.android.framework.lang.CSUserAction
+//
+//private const val RESOLUTION_REQUEST = 9000
+//
+//fun <T : CSActivityView<*>> T.checkPlayServices() {
+//    val apiAvailability = GoogleApiAvailability.getInstance()
+//    val result = apiAvailability.isGooglePlayServicesAvailable(this)
+//    if (result != ConnectionResult.SUCCESS) {
+//        if (apiAvailability.isUserResolvableError(result))
+//            apiAvailability.getErrorDialog(activity(), result, RESOLUTION_REQUEST)?.show()
+//                ?: snackBarError("Google Play Services missing application cannot continue",
+//                    CSUserAction("Finish") { activity().finish() })
+//        else snackBarError("Google Play Services missing application cannot continue",
+//            CSUserAction("Finish") { activity().finish() })
+//    }
+//}
