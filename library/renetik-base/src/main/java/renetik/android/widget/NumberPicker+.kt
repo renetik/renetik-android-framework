57fd5ac9f004269488a4c8fe5bf6522db8d1c08b
Rene Dohan <dohan.rene@gmail.com>
2022-06-17 00:23:27 -0500

    ...

--- library/renetik-base/src/main/java/renetik/android/widget/NumberPicker+.kt
+++ library/renetik-base/src/main/java/renetik/android/widget/NumberPicker+.kt
@@ -1,24 +1,19 @@
 package renetik.android.widget
 
-import android.graphics.drawable.ColorDrawable
 import android.widget.NumberPicker
 import android.widget.NumberPicker.FOCUS_BEFORE_DESCENDANTS
 import android.widget.NumberPicker.FOCUS_BLOCK_DESCENDANTS
-import androidx.annotation.ColorInt
-import renetik.android.core.lang.Func
-import renetik.android.event.registration.CSRegistration
-import renetik.android.event.registration.pause
-import renetik.android.event.property.CSEventProperty
 import renetik.android.core.kotlin.asStringArray
 import renetik.android.core.kotlin.collections.hasItems
 import renetik.android.core.kotlin.collections.index
-import renetik.android.core.kotlin.setPrivateField2
+import renetik.android.core.lang.Func
+import renetik.android.event.property.CSEventProperty
+import renetik.android.event.registration.CSRegistration
+import renetik.android.event.registration.pause
 
-@Deprecated("Impossible to style this widget , use CSNumberPicker")
 fun <Row : Any> NumberPicker.loadData(data: List<Row>, selected: Row? = null) =
     loadData(data, selectedIndex = data.index(selected) ?: 0)
 
-@Deprecated("Impossible to style this widget , use CSNumberPicker")
 fun <Row : Any> NumberPicker.loadData(data: List<Row>, selectedIndex: Int = 0) = apply {
     if (data.hasItems) {
         minValue = 0
@@ -35,28 +30,22 @@ fun <Row : Any> NumberPicker.loadData(data: List<Row>, selectedIndex: Int = 0) =
     }
 }
 
-@Deprecated("Impossible to style this widget , use CSNumberPicker")
 fun NumberPicker.onValueChange(function: Func) = apply {
     setOnValueChangedListener { _, _, _ -> function() }
 }
 
-@Deprecated("Impossible to style this widget , use CSNumberPicker")
 fun NumberPicker.circulate(circulate: Boolean) = apply {
     wrapSelectorWheel = circulate
 }
 
-@Deprecated("Impossible to style this widget , use CSNumberPicker")
 fun NumberPicker.disableTextEditing(disable: Boolean) = apply {
     descendantFocusability = if (disable) FOCUS_BLOCK_DESCENDANTS else FOCUS_BEFORE_DESCENDANTS
 }
 
-@Deprecated("Impossible to style this widget , use CSNumberPicker")
 fun NumberPicker.min(value: Int) = apply { minValue = value }
 
-@Deprecated("Impossible to style this widget , use CSNumberPicker")
 fun NumberPicker.max(value: Int) = apply { maxValue = value }
 
-@Deprecated("Impossible to style this widget , use CSNumberPicker")
 @JvmName("valuePropertyInt")
 fun NumberPicker.value(property: CSEventProperty<Int>): CSRegistration {
     val registration = value(property) { it }
@@ -64,18 +53,8 @@ fun NumberPicker.value(property: CSEventProperty<Int>): CSRegistration {
     return registration
 }
 
-@Deprecated("Impossible to style this widget , use CSNumberPicker")
 fun <T> NumberPicker.value(property: CSEventProperty<T>, function: (T) -> Int)
         : CSRegistration {
     value = function(property.value)
     return property.onChange { value = function(property.value) }
 }
-
-@Deprecated("Impossible to style this widget , use CSNumberPicker")
-// setSelectionDividerHeight requires api 29
-fun NumberPicker.selectionDividerHeight(height: Int) =
-    setPrivateField2("mSelectionDividerHeight", height)
-
-@Deprecated("Impossible to style this widget , use CSNumberPicker")
-fun NumberPicker.selectionDividerColor(@ColorInt color: Int) =
-    setPrivateField2("mSelectionDivider", ColorDrawable(color))
