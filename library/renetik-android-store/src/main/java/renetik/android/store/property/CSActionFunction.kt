12a0cc1f615d74cfb8a2647577a3c0202e5837f0
Rene Dohan <dohan.rene@gmail.com>
2022-06-17 20:09:58 -0500

    ...

--- library/renetik-android-store/src/main/java/renetik/android/store/property/CSActionFunction.kt
+++ library/renetik-android-store/src/main/java/renetik/android/store/property/CSActionFunction.kt
@@ -1,14 +1,14 @@
 package renetik.android.store.property
 
 import renetik.android.event.property.CSActionInterface
-import renetik.android.store.CSStore
+import renetik.android.store.CSStore.Companion.store
 import renetik.android.store.property.value.CSBooleanValueStoreEventProperty
 
 class CSActionFunction {
     companion object {
         var actionsDisabled = false
         fun action(id: String, function: ((Boolean) -> Unit)? = null): CSActionInterface =
-            object : CSBooleanValueStoreEventProperty(CSStore.store, id, false, function) {
+            object : CSBooleanValueStoreEventProperty(store, id, false, function) {
                 override var _value = if (!actionsDisabled) load() else false
             }
     }
