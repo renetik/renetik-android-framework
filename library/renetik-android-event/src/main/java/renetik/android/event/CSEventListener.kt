8686a3d12e740cfc9ab946efa4a41717ef80ba74
Rene Dohan <dohan.rene@gmail.com>
2022-06-21 23:40:13 -0500

    ...

--- library/renetik-android-event/src/main/java/renetik/android/event/CSEventListener.kt
+++
@@ -1,7 +0,0 @@
-package renetik.android.event
-
-import renetik.android.event.registration.CSRegistration
-
-interface CSEventListener<T> : CSRegistration {
-    fun onEvent(argument: T)
-}
--- library/renetik-android-event/src/main/java/renetik/android/event/property/CSSynchronizedProperty.kt
+++
@@ -1,8 +0,0 @@
-package renetik.android.event.property
-
-import renetik.android.core.lang.CSSynchronizedValue
-import renetik.android.core.lang.property.CSProperty
-
-interface CSSynchronizedProperty<T> : CSSynchronizedValue<T>, CSProperty<T>
-
-interface CSSynchronizedEventProperty<T> : CSSynchronizedProperty<T>, CSEventProperty<T>
