9e83dc9d677e10ec5336754cb35e3d3d29eee5f0
Rene Dohan <dohan.rene@gmail.com>
2022-06-22 15:33:20 -0500

    ...

--- library/renetik-android-store/src/main/java/renetik/android/store/json/CSStoreJsonObject+.kt
+++
@@ -1,8 +0,0 @@
-package renetik.android.store.json
-
-fun CSStoreJsonObject.getObject(key: String) = getMap(key)?.let(::CSStoreJsonObject)
-
-fun CSStoreJsonObject.reload(data: Map<String, Any?>) = bulkSave().use {
-    clear()
-    load(data)
-}
