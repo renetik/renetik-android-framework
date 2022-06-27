00550058c009255ea4b0ba08c93f7a3d0c182778
Rene Dohan <dohan.rene@gmail.com>
2022-06-27 09:32:28 -0500

    ...

--- library/renetik-android-imaging/src/main/java/renetik/android/imaging/extensions/Uri+CSImaging.kt
+++ library/renetik-android-imaging/src/main/java/renetik/android/imaging/extensions/Uri+CSImaging.kt
@@ -10,77 +10,80 @@ import android.net.Uri
 import android.provider.MediaStore.Images.Media.ORIENTATION
 import androidx.exifinterface.media.ExifInterface
 import androidx.exifinterface.media.ExifInterface.*
-import renetik.android.core.extensions.content.openInputStream
 import renetik.android.core.CSApplication.Companion.app
+import renetik.android.core.extensions.content.openInputStream
 import renetik.android.core.lang.catchAllError
 import java.io.OutputStream
 import kotlin.math.max
 
 fun Uri.resizeImage(maxTargetWidth: Int, maxTargetHeight: Int, output: OutputStream) = apply {
-    catchAllError {
-        val decodeBounds = Options()
-        app.openInputStream(this).use {
-            decodeBounds.inJustDecodeBounds = true
-            decodeStream(it, null, decodeBounds)
-        }
-        app.openInputStream(this).use { input ->
-            val options = Options()
-            options.inSampleSize = max(
-                decodeBounds.outWidth / maxTargetWidth,
-                decodeBounds.outHeight / maxTargetHeight
-            )
-            decodeStream(input, null, options)?.let { roughBitmap: Bitmap ->
-                val scaledBitmap = roughBitmap.scale(maxTargetWidth, maxTargetHeight)
-                val rotatedBitmap = createBitmap(
-                    scaledBitmap, 0, 0,
-                    scaledBitmap.width, scaledBitmap.height,
-                    createFixOrientationMatrix(), true
-                )
-                scaledBitmap.recycle()
-                output.use {
-                    rotatedBitmap.compress(JPEG, 80, it)
-                    rotatedBitmap.recycle()
-                }
-            }
-        }
-    }
+	catchAllError {
+		val decodeBounds = Options()
+		app.openInputStream(this).use {
+			decodeBounds.inJustDecodeBounds = true
+			decodeStream(it, null, decodeBounds)
+		}
+		app.openInputStream(this).use { input ->
+			val options = Options()
+			options.inSampleSize = max(
+				decodeBounds.outWidth / maxTargetWidth,
+				decodeBounds.outHeight / maxTargetHeight
+			)
+			decodeStream(input, null, options)?.let { roughBitmap: Bitmap ->
+				val scaledBitmap = roughBitmap.scale(maxTargetWidth, maxTargetHeight)
+				val rotatedBitmap = createBitmap(
+					scaledBitmap, 0, 0,
+					scaledBitmap.width, scaledBitmap.height,
+					createFixOrientationMatrix(), true
+				)
+				scaledBitmap.recycle()
+				output.use {
+					rotatedBitmap.compress(JPEG, 80, it)
+					rotatedBitmap.recycle()
+				}
+			}
+		}
+	}
 }
 
 
 fun Uri.createFixOrientationMatrix(): Matrix {
-    val matrix = Matrix()
-    catchAllError {
-        val orientation = ExifInterface(app.openInputStream(this)!!)
-            .getAttributeInt(TAG_ORIENTATION, ORIENTATION_NORMAL)
-        when (orientation) {
-            ORIENTATION_FLIP_HORIZONTAL -> matrix.setScale(-1f, 1f)
-            ORIENTATION_ROTATE_180 -> matrix.setRotate(180f)
-            ORIENTATION_FLIP_VERTICAL -> {
-                matrix.setRotate(180f)
-                matrix.postScale(-1f, 1f)
-            }
-            ORIENTATION_TRANSPOSE -> {
-                matrix.setRotate(90f)
-                matrix.postScale(-1f, 1f)
-            }
-            ORIENTATION_ROTATE_90 -> matrix.setRotate(90f)
-            ORIENTATION_TRANSVERSE -> {
-                matrix.setRotate(-90f)
-                matrix.postScale(-1f, 1f)
-            }
-            ORIENTATION_ROTATE_270 -> matrix.setRotate(-90f)
-        }
-        if (orientation == ORIENTATION_UNDEFINED) fixRotationByContentQuery(matrix)
-    }
-    return matrix
+	val matrix = Matrix()
+	catchAllError {
+		val orientation = ExifInterface(app.openInputStream(this)!!)
+			.getAttributeInt(TAG_ORIENTATION, ORIENTATION_NORMAL)
+		when (orientation) {
+			ORIENTATION_FLIP_HORIZONTAL -> matrix.setScale(-1f, 1f)
+			ORIENTATION_ROTATE_180 -> matrix.setRotate(180f)
+			ORIENTATION_FLIP_VERTICAL -> {
+				matrix.setRotate(180f)
+				matrix.postScale(-1f, 1f)
+			}
+			ORIENTATION_TRANSPOSE -> {
+				matrix.setRotate(90f)
+				matrix.postScale(-1f, 1f)
+			}
+			ORIENTATION_ROTATE_90 -> matrix.setRotate(90f)
+			ORIENTATION_TRANSVERSE -> {
+				matrix.setRotate(-90f)
+				matrix.postScale(-1f, 1f)
+			}
+			ORIENTATION_ROTATE_270 -> matrix.setRotate(-90f)
+		}
+		if (orientation == ORIENTATION_UNDEFINED) fixRotationByContentQuery(matrix)
+	}
+	return matrix
 }
 
 fun Uri.fixRotationByContentQuery(matrix: Matrix) {
-    val orientationColumn = arrayOf(ORIENTATION)
-    val cur = app.contentResolver.query(this, orientationColumn, null, null, null)
-    cur?.use {
-        var orientation = -1
-        if (cur.moveToFirst()) orientation = cur.getInt(cur.getColumnIndex(orientationColumn[0]))
-        matrix.postRotate(orientation.toFloat())
-    }
+	val orientationColumn = arrayOf(ORIENTATION)
+	val cur = app.contentResolver.query(this, orientationColumn, null, null, null)
+	cur?.use {
+		var orientation = -1
+		if (cur.moveToFirst()) {
+			val columnIndex = cur.getColumnIndex(orientationColumn[0])
+			if (columnIndex >= 0) orientation = cur.getInt(columnIndex)
+		}
+		matrix.postRotate(orientation.toFloat())
+	}
 }
