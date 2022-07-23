e1fe3f3c708f70512151b6f6aaadf60883d6af82
Rene Dohan <dohan.rene@gmail.com>
2022-07-23 13:48:39 -0500

    releaseModuleToGithub:1.9.38

--- library/renetik-android-ui/src/main/java/renetik/android/ui/extensions/widget/CompoundButton+.kt
+++ library/renetik-android-ui/src/main/java/renetik/android/ui/extensions/widget/CompoundButton+.kt
@@ -1,8 +1,7 @@
 package renetik.android.ui.extensions.widget
 
-import android.content.res.ColorStateList
+import android.content.res.ColorStateList.valueOf
 import android.widget.CompoundButton
-import android.widget.CompoundButton.OnCheckedChangeListener
 import androidx.annotation.ColorInt
 import renetik.android.event.property.CSProperty
 import renetik.android.event.property.action
@@ -12,27 +11,19 @@ import renetik.android.event.registration.CSRegistration.Companion.CSRegistratio
 import renetik.android.event.registration.paused
 import renetik.android.event.registration.start
 
-fun CompoundButton.onChange(function: (CompoundButton) -> Unit): CSRegistration {
-    val listener = OnCheckedChangeListener { buttonView, _ -> function(buttonView) }
-    return CSRegistration(onResume = { setOnCheckedChangeListener(listener) },
-        onPause = { setOnCheckedChangeListener(null) }).start()
-}
+fun CompoundButton.onChange(function: (CompoundButton) -> Unit) = CSRegistration(onResume = {
+    setOnCheckedChangeListener { buttonView, _ -> if (it.isActive) function(buttonView) }
+}, onPause = { setOnCheckedChangeListener(null) }).start()
 
 fun CompoundButton.buttonTint(@ColorInt value: Int?) = apply {
-    buttonTintList = if (value != null) ColorStateList.valueOf(value) else null
+    buttonTintList = value?.let(::valueOf)
 }
 
-fun CompoundButton.checked(condition: Boolean = true) = apply {
-    isChecked = condition
-}
-
-fun CompoundButton.isCheckedIf(condition: Boolean) = apply {
-    isChecked = condition
-}
-
-fun CompoundButton.isCheckedIfNot(condition: Boolean) = apply {
-    isChecked = !condition
-}
+fun CompoundButton.checked(condition: Boolean = true) = apply { isChecked = condition }
+fun CompoundButton.isCheckedIf(condition: Boolean) = apply { isChecked = condition }
+fun CompoundButton.isCheckedIfNot(condition: Boolean) = apply { isChecked = !condition }
+fun CompoundButton.setOn() = apply { isChecked = true }
+fun CompoundButton.setOff() = apply { isChecked = false }
 
 fun CompoundButton.isCheckedIfNot(property: CSProperty<Boolean>): CSRegistration {
     lateinit var propertyRegistration: CSRegistration
@@ -48,21 +39,14 @@ fun CompoundButton.isCheckedIf(property: CSProperty<Boolean>): CSRegistration {
     return CSRegistration(propertyRegistration, buttonRegistration)
 }
 
-fun <T> CompoundButton.isCheckedIf(property1: CSProperty<T>, property2: CSProperty<*>,
-                                   condition: (T) -> Boolean) =
-    isCheckedIf(property1, property2) { first, _ -> condition(first) }
+fun <T> CompoundButton.isCheckedIf(
+    property1: CSProperty<T>, property2: CSProperty<*>, condition: (T) -> Boolean)
+        : CSRegistration = isCheckedIf(property1, property2) { first, _ -> condition(first) }
 
-fun <T, V> CompoundButton.isCheckedIf(property1: CSProperty<T>, property2: CSProperty<V>,
-                                      condition: (T, V) -> Boolean): CSRegistration {
+fun <T, V> CompoundButton.isCheckedIf(
+    property1: CSProperty<T>, property2: CSProperty<V>,
+    condition: (T, V) -> Boolean): CSRegistration {
     fun update() = isCheckedIf(condition(property1.value, property2.value))
     update()
     return CSRegistration(property1.onChange(::update), property2.onChange(::update))
-}
-
-fun CompoundButton.setOn() {
-    isChecked = true
-}
-
-fun CompoundButton.setOff() {
-    isChecked = false
 }
