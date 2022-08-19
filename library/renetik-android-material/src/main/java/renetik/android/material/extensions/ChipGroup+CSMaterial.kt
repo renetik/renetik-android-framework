496488294c0c8765beae4d470753dc9a2219cd68
Rene Dohan <dohan.rene@gmail.com>
2022-08-19 14:16:00 +0200

    Update ChipGroup+CSMaterial.kt

--- library/renetik-android-material/src/main/java/renetik/android/material/extensions/ChipGroup+CSMaterial.kt
+++ library/renetik-android-material/src/main/java/renetik/android/material/extensions/ChipGroup+CSMaterial.kt
@@ -2,6 +2,7 @@ package renetik.android.material.extensions
 
 import com.google.android.material.chip.ChipGroup
 
+@Suppress("DEPRECATION")
 fun <T : ChipGroup> T.onChange(onChange: (view: T) -> Unit): T {
     setOnCheckedChangeListener { _, _ -> onChange(this) }
     return this
