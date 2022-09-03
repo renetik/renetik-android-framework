0ac3f909e468737831c9f76aa26d729d5c0ad106
Rene Dohan <dohan.rene@gmail.com>
2022-09-03 22:40:40 +0200

    Update CSHasRegistrationsHasVisibility+.kt

--- library/renetik-android-ui/src/main/java/renetik/android/ui/protocol/CSHasRegistrationsHasVisibility+.kt
+++ library/renetik-android-ui/src/main/java/renetik/android/ui/protocol/CSHasRegistrationsHasVisibility+.kt
@@ -5,22 +5,36 @@ import renetik.android.event.registration.CSRegistration
 import renetik.android.event.registration.cancel
 import renetik.android.event.registration.register
 
-fun <T> T.registerUntilHide(registration: CSRegistration?)
+fun <T> T.registerUntilHide(registration: CSRegistration): CSRegistration
         where T : CSHasRegistrations, T : CSVisibility {
-    if (registration == null) return
     register(registration)
     register(onHiding { onHidingRegistration ->
         cancel(onHidingRegistration)
         cancel(registration)
     })
+    return registration
 }
 
-fun <T> T.registerUntilShow(registration: CSRegistration?)
+@JvmName("registerUntilHideRegistrationNullable")
+fun <T> T.registerUntilHide(registration: CSRegistration?): CSRegistration?
         where T : CSHasRegistrations, T : CSVisibility {
-    if (registration == null) return
+    if (registration == null) return null
+    return registerUntilHide(registration)
+}
+
+fun <T> T.registerUntilShow(registration: CSRegistration): CSRegistration
+        where T : CSHasRegistrations, T : CSVisibility {
     register(registration)
     register(onShowing { onShowingRegistration ->
         cancel(onShowingRegistration)
         cancel(registration)
     })
+    return registration
+}
+
+@JvmName("registerUntilShowRegistrationNullable")
+fun <T> T.registerUntilShow(registration: CSRegistration?): CSRegistration?
+        where T : CSHasRegistrations, T : CSVisibility {
+    if (registration == null) return null
+    return registerUntilShow(registration)
 }
