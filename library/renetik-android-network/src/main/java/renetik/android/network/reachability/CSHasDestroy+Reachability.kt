90821440f36c3765c0fb4bf3d96a4ebb30b3de09
Rene Dohan <dohan.rene@gmail.com>
2022-09-05 13:47:15 +0200

    Update CSHasDestroy+Reachability.kt

--- library/renetik-android-network/src/main/java/renetik/android/network/reachability/CSHasDestroy+Reachability.kt
+++ library/renetik-android-network/src/main/java/renetik/android/network/reachability/CSHasDestroy+Reachability.kt
@@ -1,11 +1,11 @@
 package renetik.android.network.reachability
 
-import renetik.android.event.listenOnce
 import renetik.android.event.common.CSHasDestroy
+import renetik.android.event.listenOnce
 
-fun CSHasDestroy.onInternetConnected(function: () -> Unit): CSReachability {
-    val reachability = CSReachability().start()
-    reachability.eventOnConnected.listenOnce { function() }
-    eventDestroy.listen { reachability.stop() }
-    return reachability
-}
+//fun CSHasDestroy.onInternetConnected(function: () -> Unit): CSReachability {
+//    val reachability = CSReachability().start()
+//    reachability.eventOnConnected.listenOnce { function() }
+//    eventDestroy.listenOnce { reachability.stop() }
+//    return reachability
+//}
