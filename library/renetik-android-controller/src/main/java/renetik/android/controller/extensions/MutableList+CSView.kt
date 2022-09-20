279a916f71c1eef089cf3ed93a3fb86b819137cd
Rene Dohan <dohan.rene@gmail.com>
2022-09-21 00:02:32 +0200

    func MutableList<Item>.updated

--- library/renetik-android-controller/src/main/java/renetik/android/controller/extensions/MutableList+CSView.kt
+++ library/renetik-android-controller/src/main/java/renetik/android/controller/extensions/MutableList+CSView.kt
@@ -10,9 +10,10 @@ import renetik.android.event.registration.CSRegistration
 
 fun <Item : CSView<*>> MutableList<Item>.updated(
     group: ViewGroup, property: CSProperty<Int>,
+    fromStart: Boolean = false,
     function: (index: Int) -> Item): CSRegistration =
     property.action { value ->
         size.update(value,
-            onAdd = { index -> group.add(put(function(index))) },
+            onAdd = { index -> group.add(put(function(index)), if (fromStart) 0 else -1) },
             onRemove = { index -> group.remove(removeAt(index)) })
     }
