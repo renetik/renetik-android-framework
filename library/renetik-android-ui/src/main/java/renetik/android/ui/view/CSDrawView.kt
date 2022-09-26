16b0a30a93abad9820501c4815135c6486664fd0
Rene Dohan <dohan.rene@gmail.com>
2022-09-26 02:47:13 +0200

    draw view

---
+++ library/renetik-android-ui/src/main/java/renetik/android/ui/view/CSDrawView.kt
@@ -0,0 +1,84 @@
+package renetik.android.ui.view
+
+import android.content.Context
+import android.graphics.Canvas
+import android.util.AttributeSet
+import android.view.MotionEvent
+import android.view.View
+import android.view.View.MeasureSpec.EXACTLY
+import android.view.View.MeasureSpec.makeMeasureSpec
+import android.widget.FrameLayout
+import renetik.android.ui.R
+import renetik.android.event.CSEvent.Companion.event
+import renetik.android.event.fire
+
+
+open class CSDrawView @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0, defStyleRes: Int = 0
+) : View(context, attrs, defStyleAttr, defStyleRes), CSHasTouchEvent {
+
+    private val minWidth: Int
+    private val maxWidth: Int
+    var dispatchState: Boolean
+    val eventOnDraw = event<Canvas>()
+    var onDispatchTouchEvent: ((event: MotionEvent) -> Boolean)? = null
+    override val self: View get() = this
+    override var onTouchEvent: ((event: MotionEvent) -> Boolean)? = null
+    var eventOnLayout = event()
+
+    init {
+        val attributes =
+            context.theme.obtainStyledAttributes(attrs, R.styleable.CSLayout, 0, 0)
+        try {
+            minWidth = attributes.getDimensionPixelSize(R.styleable.CSLayout_minWidth, -1)
+            maxWidth = attributes.getDimensionPixelSize(R.styleable.CSLayout_maxWidth, -1)
+            dispatchState = attributes.getBoolean(R.styleable.CSLayout_dispatchState, true)
+        } finally {
+            attributes.recycle()
+        }
+    }
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+        if (minWidth != -1 && measuredWidth < minWidth) {
+            super.onMeasure(makeMeasureSpec(minWidth, EXACTLY), heightMeasureSpec)
+        } else if (maxWidth != -1 && measuredWidth > maxWidth) {
+            super.onMeasure(makeMeasureSpec(maxWidth, EXACTLY), heightMeasureSpec)
+        }
+    }
+
+    override fun dispatchSetActivated(activated: Boolean) {
+        if (dispatchState) super.dispatchSetActivated(activated)
+    }
+
+    override fun dispatchSetSelected(selected: Boolean) {
+        if (dispatchState) super.dispatchSetSelected(selected)
+    }
+
+    override fun dispatchSetPressed(pressed: Boolean) {
+        if (dispatchState) super.dispatchSetPressed(pressed)
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        eventOnDraw.fire(canvas)
+    }
+
+    override fun dispatchTouchEvent(event: MotionEvent): Boolean {
+        if (!isEnabled) return false
+        val handled = onDispatchTouchEvent?.invoke(event) ?: false
+        return if (!handled) super.dispatchTouchEvent(event) else true
+    }
+
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        if (!isEnabled) return false
+        val handled = onTouchEvent?.invoke(event) ?: false
+        return if (!handled) super.onTouchEvent(event) else true
+    }
+
+    override fun onLayout(changed: Boolean, left: Int, top: Int, right: Int, bottom: Int) {
+        super.onLayout(changed, left, top, right, bottom)
+        eventOnLayout.fire()
+    }
+}
