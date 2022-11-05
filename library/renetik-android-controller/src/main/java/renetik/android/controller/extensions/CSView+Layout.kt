ec9abca9d5a9d3beebd98613d4296d75f44e7ccb
Rene Dohan <dohan.rene@gmail.com>
2022-11-05 20:37:51 +0100

    ...

--- library/renetik-android-controller/src/main/java/renetik/android/controller/extensions/CSView+Layout.kt
+++ library/renetik-android-controller/src/main/java/renetik/android/controller/extensions/CSView+Layout.kt
@@ -3,22 +3,23 @@ package renetik.android.controller.extensions
 import android.view.View
 import renetik.android.core.extensions.content.displayWidth
 import renetik.android.core.extensions.content.dpToPixel
-import renetik.android.controller.base.CSView
-import renetik.android.core.math.CSPoint
 import renetik.android.core.kotlin.primitives.isSet
+import renetik.android.core.math.CSPoint
 import renetik.android.ui.extensions.view.locationOnScreen
+import renetik.android.ui.protocol.CSViewInterface
 
-val CSView<*>.layoutWidth get() = this.view.layoutParams.width
-val CSView<*>.layoutHeight get() = this.view.layoutParams.height
-val CSView<*>.left get() = this.view.left
-val CSView<*>.top get() = this.view.top
-val CSView<*>.center get() = CSPoint(left + width / 2, top + height / 2)
-val CSView<*>.width get() = view.width
-val CSView<*>.height get() = view.height
-val CSView<*>.locationOnScreen get() = view.locationOnScreen
+val CSViewInterface.layoutWidth get() = this.view.layoutParams.width
+val CSViewInterface.layoutHeight get() = this.view.layoutParams.height
+val CSViewInterface.left get() = this.view.left
+val CSViewInterface.top get() = this.view.top
+val CSViewInterface.center get() = CSPoint(left + width / 2, top + height / 2)
+val CSViewInterface.width get() = view.width
+val CSViewInterface.height get() = view.height
+val CSViewInterface.locationOnScreen get() = view.locationOnScreen
 
-fun CSView<*>.setPercentAspectWidth(view: View, percent: Int) {
-    val onePercent = displayWidth / 100.toFloat()
+fun <T : CSViewInterface> T.setPercentAspectWidth(
+    view: View, percent: Int) = apply {
+    val onePercent = context.displayWidth / 100.toFloat()
     val wantedWidth = onePercent * percent
 
     val scalingFactor = wantedWidth / view.layoutParams.width
@@ -31,8 +32,9 @@ fun CSView<*>.setPercentAspectWidth(view: View, percent: Int) {
     view.layoutParams = layoutParams
 }
 
-fun CSView<*>.setPercentWidth(view: View, percent: Int, minimal: Int = 0, maximal: Int = 0) {
-    val onePercent = (displayWidth / 100).toDouble()
+fun <T : CSViewInterface> T.setPercentWidth(
+    view: View, percent: Int, minimal: Int = 0, maximal: Int = 0) = apply {
+    val onePercent = (context.displayWidth / 100).toDouble()
     var wantedSize = (onePercent * percent).toInt()
     if (minimal.isSet && wantedSize < minimal)
         wantedSize = minimal
@@ -42,32 +44,32 @@ fun CSView<*>.setPercentWidth(view: View, percent: Int, minimal: Int = 0, maxima
     view.layoutParams = layoutParams
 }
 
-fun CSView<*>.size(width: Int, height: Int) = apply {
+fun <T : CSViewInterface> T.size(width: Int, height: Int) = apply {
     width(width)
     height(height)
 }
 
-fun CSView<*>.width(value: Int) = apply {
+fun <T : CSViewInterface> T.width(value: Int) = apply {
     val params = view.layoutParams
     params.width = value
     view.layoutParams = params
 }
 
-fun CSView<*>.width(value: Float) = width(value.toInt())
+fun <T : CSViewInterface> T.width(value: Float) = width(value.toInt())
 
-fun CSView<*>.widthDp(value: Int) = apply {
+fun <T : CSViewInterface> T.widthDp(value: Int) = apply {
     val params = view.layoutParams
-    params.width = dpToPixel(value)
+    params.width = context.dpToPixel(value)
     view.layoutParams = params
 }
 
-fun CSView<*>.widthDp(value: Float) = apply {
+fun <T : CSViewInterface> T.widthDp(value: Float) = apply {
     val params = view.layoutParams
-    params.width = dpToPixel(value)
+    params.width = context.dpToPixel(value)
     view.layoutParams = params
 }
 
-fun CSView<*>.height(value: Int) = apply {
+fun <T : CSViewInterface> T.height(value: Int) = apply {
     val params = view.layoutParams
     params.height = value
     view.layoutParams = params
