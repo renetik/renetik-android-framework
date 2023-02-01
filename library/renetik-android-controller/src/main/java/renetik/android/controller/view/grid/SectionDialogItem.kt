626bdcdda17fc912a90d48dae5a428e390d5fb0f
Rene Dohan <dohan.rene@gmail.com>
2023-02-01 13:58:29 +0400

    ...

--- library/renetik-android-controller/src/main/java/renetik/android/controller/view/grid/SectionDialogItem.kt
+++ library/renetik-android-controller/src/main/java/renetik/android/controller/view/grid/SectionDialogItem.kt
@@ -1,6 +1,6 @@
 package renetik.android.controller.view.grid
 
-class SectionDialogItem<Item>(
+data class SectionDialogItem<Item>(
     val section: String? = null,
     val index: Int,
     val item: Item? = null)
