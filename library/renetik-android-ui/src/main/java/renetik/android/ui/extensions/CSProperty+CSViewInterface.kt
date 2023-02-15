326329bf18e4ee7dec1e63c7c2003c52d15d974e
Rene Dohan <dohan.rene@gmail.com>
2023-02-15 12:06:36 +0100

    ...

--- library/renetik-android-ui/src/main/java/renetik/android/ui/extensions/CSProperty+CSViewInterface.kt
+++ library/renetik-android-ui/src/main/java/renetik/android/ui/extensions/CSProperty+CSViewInterface.kt
@@ -8,13 +8,20 @@ import renetik.android.event.property.action
 import renetik.android.event.registration.CSRegistration
 import renetik.android.ui.protocol.CSViewInterface
 
-fun <Item : CSViewInterface> CSProperty<Int>.updates(
-    items: MutableList<Item>, layout: ViewGroup, fromStart: Boolean = false,
-    function: (index: Int) -> Item): CSRegistration = action { value ->
+fun <ItemView : CSViewInterface> CSProperty<Int>.updates(
+    items: MutableList<ItemView>,
+    layout: ViewGroup, fromStart: Boolean = false,
+    createItemView: (index: Int) -> ItemView
+): CSRegistration = action { value ->
     items.size.update(value,
-        onAdd = { index -> layout.add(items.put(function(index)), if (fromStart) 0 else -1) },
+        onAdd = { index ->
+            layout.add(
+                view = items.put(createItemView(index)),
+                index = if (fromStart) 0 else -1
+            )
+        },
         onRemove = { index ->
-            val view = items.removeAt(index)
-            if (!view.isDestructed) layout.remove(view)
+            val itemView: ItemView = items.removeAt(index)
+            if (!itemView.isDestructed) layout.remove(itemView)
         })
 }
