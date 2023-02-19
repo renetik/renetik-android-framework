ba8c35bcbc83c0dc3905610681bb16588b199f58
Rene Dohan <dohan.rene@gmail.com>
2023-02-19 19:58:06 +0100

    ...

--- library/renetik-android-ui/src/main/java/renetik/android/ui/extensions/view/View+Position.kt
+++ library/renetik-android-ui/src/main/java/renetik/android/ui/extensions/view/View+Position.kt
@@ -2,6 +2,7 @@ package renetik.android.ui.extensions.view
 
 import android.view.View
 import renetik.android.core.math.CSPoint
+import renetik.android.event.util.CSLater.later
 
 val <T : View> T.topFromBottom get() = superview?.let { it.height - top } ?: height
 
@@ -19,9 +20,11 @@ val View.locationInWindow: CSPoint<Int>
         return CSPoint(location[0], location[1])
     }
 
-fun View.registerToLocationOnScreen(): IntArray {
+fun View.registerToLocationOnScreen(delay: Int = 0): IntArray {
     val location = IntArray(2)
-    getLocationOnScreen(location)
-    onSizeChange { getLocationOnScreen(location) }
+    fun updateLocation() =
+        later(delay) { if (isAttachedToWindow) getLocationOnScreen(location) }
+    updateLocation()
+    onSizeChange { updateLocation() }
     return location
 }
