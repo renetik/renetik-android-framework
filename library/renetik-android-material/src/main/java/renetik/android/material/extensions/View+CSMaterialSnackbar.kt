0f6027676883b1eef63369506554a85593f68a63
Rene Dohan <dohan.rene@gmail.com>
2023-06-21 21:37:58 +0200

    ...

--- library/renetik-android-material/src/main/java/renetik/android/material/extensions/View+CSMaterialSnackbar.kt
+++ library/renetik-android-material/src/main/java/renetik/android/material/extensions/View+CSMaterialSnackbar.kt
@@ -18,7 +18,8 @@ fun View.snackBar(title: String) = snackBar(title, time = LENGTH_LONG)
 
 fun View.snackBar(
     title: String, @ColorInt backColor: Int? = null, @ColorInt textColor: Int? = null,
-    time: Int = LENGTH_LONG, action: CSButtonAction? = null): Snackbar {
+    time: Int = LENGTH_LONG, action: CSButtonAction? = null
+): Snackbar {
     val bar = make(this, title, if (action.notNull) LENGTH_INDEFINITE else time)
     action?.let {
         bar.setAction(action.title) {
@@ -28,19 +29,26 @@ fun View.snackBar(
     }
     backColor?.let { bar.view.setBackgroundColor(it) }
     textColor?.let { bar.setTextColor(it) }
+    textColor?.let { bar.setTextMaxLines(Int.MAX_VALUE) }
     textColor?.let { bar.setActionTextColor(it) }
     bar.show()
     return bar
 }
 
 fun View.snackBarWarn(title: String, action: CSButtonAction? = null) =
-    snackBar(title, backColor = context.attributeColor(colorError),
-        textColor = context.attributeColor(colorOnError), action = action)
+    snackBar(
+        title, backColor = context.attributeColor(colorError),
+        textColor = context.attributeColor(colorOnError), action = action
+    )
 
 fun View.snackBarError(title: String, action: CSButtonAction? = null) =
-    snackBar(title, backColor = context.attributeColor(colorError),
-        textColor = context.attributeColor(colorOnError), action = action)
+    snackBar(
+        title, backColor = context.attributeColor(colorError),
+        textColor = context.attributeColor(colorOnError), action = action
+    )
 
 fun View.snackBarInfo(title: String, action: CSButtonAction? = null) =
-    snackBar(title, backColor = context.attributeColor(colorPrimary),
-        textColor = context.attributeColor(colorOnPrimary), action = action)
+    snackBar(
+        title, backColor = context.attributeColor(colorPrimary),
+        textColor = context.attributeColor(colorOnPrimary), action = action
+    )
