29478f868346821f3c45b3d247c7edfbfb0b879c
Rene Dohan <dohan.rene@gmail.com>
2023-09-01 22:01:49 +0200

    ...

--- library/renetik-android-controller/src/main/java/renetik/android/controller/navigation/CSNavigationItem.kt
+++ library/renetik-android-controller/src/main/java/renetik/android/controller/navigation/CSNavigationItem.kt
@@ -1,15 +1,15 @@
 package renetik.android.controller.navigation
 
-import renetik.android.controller.navigation.CSNavigationAnimation.SlideInRight
-import renetik.android.controller.navigation.CSNavigationAnimation.SlideOutLeft
+import renetik.android.controller.navigation.CSNavigationAnimation.FadeIn
+import renetik.android.controller.navigation.CSNavigationAnimation.FadeOut
 import renetik.android.core.lang.value.CSValue
 import renetik.android.core.lang.value.CSValue.Companion.value
 
 interface CSNavigationItem {
     val isNavigationBackPressedAllowed get() = true
     val isFullscreenNavigationItem: CSValue<Boolean> get() = value(true)
-    val pushAnimation: CSNavigationAnimation get() = SlideInRight
-    val popAnimation: CSNavigationAnimation get() = SlideOutLeft
+    val pushAnimation: CSNavigationAnimation get() = FadeIn
+    val popAnimation: CSNavigationAnimation get() = FadeOut
     fun onViewControllerPush(navigation: CSNavigationView) = Unit
     fun onViewControllerPop(navigation: CSNavigationView) = Unit
 }
