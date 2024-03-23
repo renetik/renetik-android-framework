923d1ee11c2add790b14cb840a299895f5faa278
Rene Dohan <dohan.rene@gmail.com>
2024-03-24 01:16:07 +0800

    adsr stk UI WIP

--- library/renetik-android-ui/src/main/java/renetik/android/ui/protocol/CSHasContextHasId.kt
+++ library/renetik-android-ui/src/main/java/renetik/android/ui/protocol/CSHasContextHasId.kt
@@ -2,5 +2,6 @@ package renetik.android.ui.protocol
 
 import renetik.android.core.lang.CSHasId
 import renetik.android.event.common.CSHasContext
+import renetik.android.event.common.CSHasRegistrationsHasDestructHasId
 
-interface CSHasContextHasId : CSHasContext, CSHasId
+interface CSHasContextHasId : CSHasContext, CSHasId, CSHasRegistrationsHasDestructHasId
