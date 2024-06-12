6a81a5a3b9feafbd9d83457d1f04f4c391a37f88
Renetik <dohan.rene@gmail.com>
2024-06-12 15:00:40 +0800

    ...

--- library/renetik-android-ui/src/main/java/renetik/android/ui/extensions/FrameLayout+CSViewInterface.kt
+++ library/renetik-android-ui/src/main/java/renetik/android/ui/extensions/FrameLayout+CSViewInterface.kt
@@ -4,6 +4,7 @@ import android.view.View
 import android.view.ViewGroup.LayoutParams
 import android.widget.FrameLayout
 import androidx.annotation.LayoutRes
+import renetik.android.core.lang.CSGuard.guard
 import renetik.android.ui.extensions.view.add
 import renetik.android.ui.extensions.view.clear
 import renetik.android.ui.protocol.CSViewInterface
@@ -22,5 +23,14 @@ fun <View : CSViewInterface> FrameLayout.set(view: View): View {
 fun <View : CSViewInterface> FrameLayout.set(view: View, params: LayoutParams): View {
     clear()
     add(view.view, params)
+    return view
+}
+
+@JvmName("setNull")
+fun <View : CSViewInterface> FrameLayout.set(
+    view: View? = null, params: LayoutParams? = null
+): View? {
+    clear()
+    view?.let { params?.let { add(view.view, params) } ?: add(it.view) }
     return view
 }
