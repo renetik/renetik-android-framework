44eea8fbe1cf6f0f0a4eb864a14804fb75d98f85
Renetik <dohan.rene@gmail.com>
2024-06-23 19:45:05 +0800

    ...

--- library/renetik-android-ui/src/main/java/renetik/android/ui/view/CSAndroidView.kt
+++ library/renetik-android-ui/src/main/java/renetik/android/ui/view/CSAndroidView.kt
@@ -2,7 +2,14 @@ package renetik.android.ui.view
 
 import android.util.AttributeSet
 import android.view.View
+import android.view.View.MeasureSpec.makeMeasureSpec
 import renetik.android.ui.R
+import renetik.android.ui.R.styleable.CSLayout_clipToOutline
+import renetik.android.ui.R.styleable.CSLayout_dispatchState
+import renetik.android.ui.R.styleable.CSLayout_maxHeight
+import renetik.android.ui.R.styleable.CSLayout_maxWidth
+import renetik.android.ui.R.styleable.CSLayout_minHeight
+import renetik.android.ui.R.styleable.CSLayout_minWidth
 
 interface CSAndroidView {
 
@@ -18,12 +25,12 @@ interface CSAndroidView {
         val attributes =
             self.context.theme.obtainStyledAttributes(attrs, R.styleable.CSLayout, 0, 0)
         try {
-            csClipToOutline = attributes.getBoolean(R.styleable.CSLayout_clipToOutline, false)
-            csMinWidth = attributes.getDimensionPixelSize(R.styleable.CSLayout_minWidth, -1)
-            csMaxWidth = attributes.getDimensionPixelSize(R.styleable.CSLayout_maxWidth, -1)
-            csMinHeight = attributes.getDimensionPixelSize(R.styleable.CSLayout_minHeight, -1)
-            csMaxHeight = attributes.getDimensionPixelSize(R.styleable.CSLayout_maxHeight, -1)
-            csDispatchState = attributes.getBoolean(R.styleable.CSLayout_dispatchState, true)
+            csClipToOutline = attributes.getBoolean(CSLayout_clipToOutline, false)
+            csMinWidth = attributes.getDimensionPixelSize(CSLayout_minWidth, -1)
+            csMaxWidth = attributes.getDimensionPixelSize(CSLayout_maxWidth, -1)
+            csMinHeight = attributes.getDimensionPixelSize(CSLayout_minHeight, -1)
+            csMaxHeight = attributes.getDimensionPixelSize(CSLayout_maxHeight, -1)
+            csDispatchState = attributes.getBoolean(CSLayout_dispatchState, true)
         } finally {
             attributes.recycle()
         }
@@ -34,16 +41,18 @@ interface CSAndroidView {
         var heightMeasure = heightMeasureSpec
 
         if (csMinWidth != -1 && self.measuredWidth < csMinWidth) {
-            widthMeasure = View.MeasureSpec.makeMeasureSpec(csMinWidth, View.MeasureSpec.EXACTLY)
+            widthMeasure = makeMeasureSpec(csMinWidth, View.MeasureSpec.EXACTLY)
         } else if (csMaxWidth != -1 && self.measuredWidth > csMaxWidth) widthMeasure =
-            View.MeasureSpec.makeMeasureSpec(csMaxWidth, View.MeasureSpec.EXACTLY)
+            makeMeasureSpec(csMaxWidth, View.MeasureSpec.EXACTLY)
 
-        if (csMinHeight != -1 && self.measuredHeight < csMinHeight) heightMeasure =
-            View.MeasureSpec.makeMeasureSpec(csMinHeight, View.MeasureSpec.EXACTLY)
-        else if (csMaxHeight != -1 && self.measuredHeight > csMaxHeight) heightMeasure =
-            View.MeasureSpec.makeMeasureSpec(csMaxHeight, View.MeasureSpec.EXACTLY)
+        if (csMinHeight != -1 && self.measuredHeight < csMinHeight)
+            heightMeasure = makeMeasureSpec(csMinHeight, View.MeasureSpec.EXACTLY)
+        else if (csMaxHeight != -1 && self.measuredHeight > csMaxHeight)
+            heightMeasure = makeMeasureSpec(csMaxHeight, View.MeasureSpec.EXACTLY)
+
         if (widthMeasure != widthMeasureSpec || heightMeasure != heightMeasureSpec)
             return widthMeasure to heightMeasure
+
         return null
     }
 }
