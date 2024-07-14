cf2ea90089abb4bd3679b7c6364ee2f8fce9f924
Renetik <dohan.rene@gmail.com>
2024-07-14 15:13:10 +0800

    ...

--- library/renetik-android-material/src/main/java/renetik/android/material/controller/CSView+Material.kt
+++ library/renetik-android-material/src/main/java/renetik/android/material/controller/CSView+Material.kt
@@ -1,29 +1,21 @@
 package renetik.android.material.controller
 
-import com.google.android.material.chip.Chip
-import com.google.android.material.chip.ChipGroup
-import com.google.android.material.floatingactionbutton.FloatingActionButton
+import androidx.appcompat.widget.SwitchCompat
 import com.google.android.material.slider.RangeSlider
 import com.google.android.material.slider.Slider
-import com.google.android.material.switchmaterial.SwitchMaterial
 import com.google.android.material.textfield.TextInputLayout
 import renetik.android.controller.base.CSView
-import renetik.android.material.extensions.materialSwitch
-import renetik.android.material.extensions.rangeSlider
-import renetik.android.material.extensions.slider
 import renetik.android.ui.extensions.findView
 import renetik.android.ui.extensions.view.findView
 import renetik.android.ui.extensions.view.onClick
 
 fun CSView<*>.textInput(
-    id: Int, onClick: ((TextInputLayout) -> Unit)? = null) =
+    id: Int, onClick: ((TextInputLayout) -> Unit)? = null
+) =
     view.findView<TextInputLayout>(id)!!.apply {
         onClick?.let { editText?.onClick { onClick(this) } }
     }
 
-fun CSView<*>.switch(id: Int): SwitchMaterial = view.materialSwitch(id)
-fun CSView<*>.slider(id: Int): Slider = view.slider(id)
-fun CSView<*>.rangeSlider(id: Int): RangeSlider = view.rangeSlider(id)
-fun CSView<*>.chip(id: Int): Chip = findView<Chip>(id)!!
-fun CSView<*>.chipGroup(id: Int): ChipGroup = findView<ChipGroup>(id)!!
-fun CSView<*>.floatingButton(id: Int): FloatingActionButton = findView<FloatingActionButton>(id)!!
+fun CSView<*>.switch(id: Int): SwitchCompat = findView(id)!!
+fun CSView<*>.slider(id: Int): Slider = findView(id)!!
+fun CSView<*>.rangeSlider(id: Int): RangeSlider = findView(id)!!
--- library/renetik-android-material/src/main/java/renetik/android/material/extensions/Switch+CSMaterial.kt
+++ library/renetik-android-material/src/main/java/renetik/android/material/extensions/Switch+CSMaterial.kt
@@ -1,9 +1,4 @@
 package renetik.android.material.extensions
 
-import android.view.View
-import com.google.android.material.switchmaterial.SwitchMaterial
-import renetik.android.ui.extensions.view.findView
-
-fun View.materialSwitch(id: Int) = findView<SwitchMaterial>(id)!!
 
 
