fea1c8240d5cd5cb045cee7bbdf2c880326cb66e
Renetik <dohan.rene@gmail.com>
2024-07-18 12:48:56 +0800

    ...

--- library/renetik-android-controller/src/main/java/renetik/android/controller/navigation/CSNavigationItemView+.kt
+++ library/renetik-android-controller/src/main/java/renetik/android/controller/navigation/CSNavigationItemView+.kt
@@ -4,18 +4,22 @@ import android.view.Gravity.CENTER
 import android.view.View
 import android.widget.FrameLayout.LayoutParams
 import androidx.core.view.updateLayoutParams
+import renetik.android.controller.base.CSView
 import renetik.android.controller.navigation.CSNavigationItemAnimation.Fade
 import renetik.android.controller.navigation.CSNavigationItemAnimation.None
 import renetik.android.core.lang.CSLeakCanary
 import renetik.android.ui.extensions.view.matchParent
-import renetik.android.ui.extensions.view.passClicksUnder
+
+fun <T : CSNavigationItemView> T.selected(button: CSView<*>) = selected(button.view)
 
 fun <T : CSNavigationItemView> T.selected(button: View) = apply {
     button.isSelected = true
     onClose { button.isSelected = false }
 }
 
-fun <T : CSNavigationItemView> T.show(animation: CSNavigationItemAnimation = Fade) = apply {
+fun <T : CSNavigationItemView> T.show(
+    animation: CSNavigationItemAnimation = Fade
+) = apply {
     this.animation = if (CSLeakCanary.isEnabled) None else animation
     navigation!!.push(this)
     updateVisibility()
