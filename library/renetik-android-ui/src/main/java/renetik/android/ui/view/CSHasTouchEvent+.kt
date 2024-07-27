6cb35ccf30ad0c7199fc2e7a8ebd1cd735d1d41e
Renetik <dohan.rene@gmail.com>
2024-07-27 22:12:04 +0800

    ...

--- library/renetik-android-ui/src/main/java/renetik/android/ui/view/CSHasTouchEvent+.kt
+++ library/renetik-android-ui/src/main/java/renetik/android/ui/view/CSHasTouchEvent+.kt
@@ -7,35 +7,33 @@ import android.view.MotionEvent.ACTION_MOVE
 import android.view.MotionEvent.ACTION_UP
 import kotlinx.coroutines.Dispatchers.Main
 import kotlinx.coroutines.delay
-import renetik.android.core.lang.variable.CSWeakVariable.Companion.weak
 import renetik.android.event.property.CSProperty
 import renetik.android.event.registration.CSHasRegistrations
 import renetik.android.event.registration.CSRegistration
 import renetik.android.event.registration.CSRegistration.Companion.CSRegistration
 import renetik.android.event.registration.action
-import renetik.android.event.registration.laterEach
 import renetik.android.event.registration.launch
 import renetik.android.event.registration.paused
+import renetik.android.event.registration.laterEach
 import renetik.android.ui.extensions.view.onLongClick
 import renetik.android.ui.extensions.view.pressed
 import kotlin.time.Duration
 import kotlin.time.Duration.Companion.milliseconds
 import kotlin.time.Duration.Companion.seconds
 
-fun <T : CSHasTouchEvent> T.onTouch(
-    down: (isDown: Boolean) -> Unit,
+inline fun <T : CSHasTouchEvent> T.onTouch(
+    crossinline down: (isDown: Boolean) -> Unit,
 ) = apply {
-    val onDown by weak(down) // To fix leakcanary (false?) report
     onTouchEvent = {
         when (it.actionMasked) {
             ACTION_DOWN -> true.also {
                 self.pressed(true)
-                onDown?.invoke(true)
+                down(true)
             }
 
             ACTION_UP, ACTION_CANCEL -> true.also {
                 self.pressed(false)
-                onDown?.invoke(false)
+                down(false)
             }
 
             ACTION_MOVE -> true
@@ -64,65 +62,59 @@ inline fun <T : CSHasTouchEvent> T.onLongTouch(
 }
 
 
-fun <T : CSHasTouchEvent> T.onTouchMove(
-    move: (event: MotionEvent) -> Unit,
+inline fun <T : CSHasTouchEvent> T.onTouchMove(
+    crossinline move: (event: MotionEvent) -> Unit,
 ) = apply {
-    val onMove by weak(move) // To fix leakcanary (false?) report
     onTouchEvent = { event ->
         when (event.actionMasked) {
             ACTION_DOWN -> true
             ACTION_UP, ACTION_CANCEL -> true
-            ACTION_MOVE -> true.also { onMove?.invoke(event) }
+            ACTION_MOVE -> true.also { move(event) }
             else -> false
         }
     }
 }
 
-fun <T : CSHasTouchEvent> T.onTouch(
-    down: (event: MotionEvent) -> Unit,
-    move: (event: MotionEvent) -> Unit,
+inline fun <T : CSHasTouchEvent> T.onTouch(
+    crossinline down: (event: MotionEvent) -> Unit,
+    crossinline move: (event: MotionEvent) -> Unit,
 ) = apply {
-    val onDown by weak(down) // To fix leakcanary (false?) report
-    val onMove by weak(move) // To fix leakcanary (false?) report
     onTouchEvent = { event ->
         when (event.actionMasked) {
-            ACTION_DOWN -> true.also { onDown?.invoke(event) }
+            ACTION_DOWN -> true.also { down(event) }
             ACTION_UP, ACTION_CANCEL -> true
-            ACTION_MOVE -> true.also { onMove?.invoke(event) }
+            ACTION_MOVE -> true.also { move(event) }
             else -> false
         }
     }
 }
 
 
-fun <T : CSHasTouchEvent> T.onTouch(
-    down: (event: MotionEvent) -> Unit,
-    move: (event: MotionEvent) -> Unit,
-    up: (event: MotionEvent) -> Unit,
+inline fun <T : CSHasTouchEvent> T.onTouch(
+    crossinline down: (event: MotionEvent) -> Unit,
+    crossinline move: (event: MotionEvent) -> Unit,
+    crossinline up: (event: MotionEvent) -> Unit,
 ) = apply {
-    val onDown by weak(down)
-    val onMove by weak(move)
-    val onUp by weak(up)
     onTouchEvent = { event ->
         when (event.actionMasked) {
-            ACTION_DOWN -> true.also { onDown?.invoke(event) }
-            ACTION_UP, ACTION_CANCEL -> true.also { onUp?.invoke(event) }
-            ACTION_MOVE -> true.also { onMove?.invoke(event) }
+            ACTION_DOWN -> true.also { down(event) }
+            ACTION_UP, ACTION_CANCEL -> true.also { up(event) }
+            ACTION_MOVE -> true.also { move(event) }
             else -> false
         }
     }
 }
 
 
-@JvmName("onTouchDownOrMove") fun <T : CSHasTouchEvent> T.onTouch(
-    downOrMove: (event: MotionEvent) -> Unit,
+@JvmName("onTouchDownOrMove")
+inline fun <T : CSHasTouchEvent> T.onTouch(
+    crossinline downOrMove: (event: MotionEvent) -> Unit,
 ) = apply {
-    val onDownOrMove by weak(downOrMove)
     onTouchEvent = { event ->
         when (event.actionMasked) {
-            ACTION_DOWN -> true.also { onDownOrMove?.invoke(event) }
+            ACTION_DOWN -> true.also { downOrMove(event) }
             ACTION_UP, ACTION_CANCEL -> true
-            ACTION_MOVE -> true.also { onDownOrMove?.invoke(event) }
+            ACTION_MOVE -> true.also { downOrMove(event) }
             else -> false
         }
     }
@@ -136,15 +128,13 @@ inline fun <T : CSHasTouchEvent> T.onTouchUp(
     crossinline up: () -> Unit,
 ) = onTouch(down = { isDown -> if (!isDown) up() })
 
-fun <T : CSHasTouchEvent> T.onTouchUp(
-    up: () -> Unit, cancel: () -> Unit,
+inline fun <T : CSHasTouchEvent> T.onTouchUp(
+    crossinline up: () -> Unit, crossinline cancel: () -> Unit,
 ) = apply {
-    val onUp by weak(up)
-    val onCancel by weak(cancel)
     onTouchEvent = {
         when (it.actionMasked) {
-            ACTION_UP -> false.also { onUp?.invoke() }
-            ACTION_CANCEL -> false.also { onCancel?.invoke() }
+            ACTION_UP -> false.also { up() }
+            ACTION_CANCEL -> false.also { cancel() }
             else -> false
         }
     }
@@ -177,16 +167,14 @@ inline fun <T : CSHasTouchEvent> T.onTouch(
     crossinline up: () -> Unit,
 ) = onTouch(down = { isDown -> if (isDown) down() else up() })
 
-fun <T : CSHasTouchEvent> T.toggleActiveIf(
-    property: CSProperty<Boolean>): CSRegistration {
+fun <T : CSHasTouchEvent> T.toggleActiveIf(property: CSProperty<Boolean>): CSRegistration {
     setToggleActive(property.value)
     val propertyOnChange = property.onChange { setToggleActive(property.value) }
     onTouchActiveToggle { on -> propertyOnChange.paused { property.value(on) } }
     return propertyOnChange
 }
 
-fun <T : CSHasTouchEvent> T.toggleSelectedIf(
-    property: CSProperty<Boolean>): CSRegistration {
+fun <T : CSHasTouchEvent> T.toggleSelectedIf(property: CSProperty<Boolean>): CSRegistration {
     val propertyOnChange = property.action { setToggleSelected(property.value) }
     onTouchSelectedToggle { on -> propertyOnChange.paused { property.value(on) } }
     return propertyOnChange
@@ -253,15 +241,20 @@ fun CSHasTouchEvent.onTouch(
     delay: Duration = 1.seconds,
     period: Duration = 250.milliseconds,
     repeat: () -> Unit,
-) = onTouch(parent, delay.inWholeMilliseconds.toInt(), period.inWholeMilliseconds.toInt(),
-    repeat)
+) = onTouch(
+    parent, delay.inWholeMilliseconds.toInt(),
+    period.inWholeMilliseconds.toInt(), repeat
+)
 
 fun CSHasTouchEvent.onTouch(
     parent: CSHasRegistrations,
     delay: Int, period: Int,
     repeat: () -> Unit,
-) = onTouch(parent, repeat = { repeat() }, step = { }, delay = delay, period = period,
-    until = { true }, onDone = {})
+) = onTouch(
+    parent, repeat = { repeat() },
+    step = { }, delay = delay, period = period,
+    until = { true }, onDone = {}
+)
 
 fun <T> CSHasTouchEvent.onTouch(
     parent: CSHasRegistrations,
@@ -277,15 +270,16 @@ fun <T> CSHasTouchEvent.onTouch(
         repeatCount = 0
         repeat(step(repeatCount))
         repeatRegistration?.cancel()
-        if (self.isEnabled) repeatRegistration =
-            parent.laterEach(delay, period, function = {
+        if (self.isEnabled) repeatRegistration = parent.laterEach(
+            delay, period, function = {
                 repeat(step(repeatCount))
                 repeatCount++
                 if (!until(step(repeatCount))) {
                     repeatRegistration?.cancel()
                     onDone()
                 }
-            })
+            }
+        )
     }, up = {
         repeatRegistration?.cancel()
     })
