11e904d76aa9acc2b662062e0c107400416ba3f0
Rene Dohan <dohan.rene@email.com>
2024-11-21 23:58:42 +0800

    ...

--- library/renetik-android-imaging/src/main/java/renetik/android/imaging/extensions/Bitmap+CSImaging.kt
+++ library/renetik-android-imaging/src/main/java/renetik/android/imaging/extensions/Bitmap+CSImaging.kt
@@ -4,25 +4,50 @@ import android.graphics.Bitmap
 import android.graphics.Bitmap.CompressFormat
 import android.graphics.Bitmap.CompressFormat.JPEG
 import android.graphics.Bitmap.createScaledBitmap
-import android.graphics.Matrix
-import android.graphics.RectF
+import android.graphics.Canvas
+import android.graphics.Color.alpha
+import android.graphics.Color.argb
+import android.graphics.Color.blue
+import android.graphics.Color.green
+import android.graphics.Color.red
+import android.graphics.ColorMatrix
+import android.graphics.ColorMatrixColorFilter
+import android.graphics.Paint
 import java.io.File
 
 fun Bitmap.scale(
     maxTargetWidth: Int, maxTargetHeight: Int = maxTargetWidth
 ): Bitmap {
-    val matrix = Matrix()
-    val inRect = RectF(0f, 0f, width.toFloat(), height.toFloat())
-    val outRect = RectF(0f, 0f, maxTargetWidth.toFloat(), maxTargetHeight.toFloat())
-    matrix.setRectToRect(inRect, outRect, Matrix.ScaleToFit.CENTER)
-    val values = FloatArray(9)
-    matrix.getValues(values)
-    val scaled = createScaledBitmap(
-        this, (width * values[0]).toInt(),
-        (height * values[4]).toInt(), true
-    )
-    recycle()
-    return scaled
+    val aspectRatio = width.toFloat() / height.toFloat()
+    val targetWidth: Int = if (width > height) maxTargetWidth
+    else (maxTargetHeight * aspectRatio).toInt()
+    val targetHeight: Int = if (width > height) (maxTargetWidth / aspectRatio).toInt()
+    else maxTargetHeight
+    return createScaledBitmap(this, targetWidth, targetHeight, true).also { recycle() }
+}
+
+fun Bitmap.toMonochrome(): Bitmap {
+    val grayscaleBitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
+    val canvas = Canvas(grayscaleBitmap)
+    val paint = Paint()
+    val colorMatrix = ColorMatrix()
+    colorMatrix.setSaturation(0f)
+    val filter = ColorMatrixColorFilter(colorMatrix)
+    paint.colorFilter = filter
+    canvas.drawBitmap(this, 0f, 0f, paint)
+    return grayscaleBitmap
+}
+
+fun Bitmap.toMonochromeManual(): Bitmap {
+    val monochromeBitmap = copy(Bitmap.Config.ARGB_8888, true)
+    for (y in 0 until monochromeBitmap.height) {
+        for (x in 0 until monochromeBitmap.width) {
+            val pixel = monochromeBitmap.getPixel(x, y)
+            val gray = (0.299 * red(pixel) + 0.587 * green(pixel) + 0.114 * blue(pixel)).toInt()
+            monochromeBitmap.setPixel(x, y, argb(alpha(pixel), gray, gray, gray))
+        }
+    }
+    return monochromeBitmap
 }
 
 
