385b309228b331c02961fe1a385818e755fcfa24
Rene Dohan <dohan.rene@email.com>
2024-12-28 14:41:01 +0800

    ...

--- library/renetik-android-controller/src/main/java/renetik/android/controller/view/grid/item/CSItemSection.kt
+++ library/renetik-android-controller/src/main/java/renetik/android/controller/view/grid/item/CSItemSection.kt
@@ -10,9 +10,10 @@ data class CSItemSection<Item>(
     val title: String,
     val description: String = "",
     var items: List<Item> = emptyList(),
-    val store: CSStore = EmptyStore
+    val store: CSStore = EmptyStore,
+    val isCollapsedDefault: Boolean = false
 ) {
-    val isCollapsed = store.property(id, false)
+    val isCollapsed = store.property(id, isCollapsedDefault)
 
     constructor(
         title: String, description: String = "", items: List<Item>
@@ -26,6 +27,11 @@ data class CSItemSection<Item>(
         inline fun <reified Item> dialogSection(
             title: String, subtitle: String = "", items: List<Item>
         ) = CSItemSection(title, subtitle, items)
+
+        inline fun <reified Item> dialogSection(
+            store: CSStore, id: String, title: String,
+            items: List<Item>, isCollapsedDefault: Boolean = false
+        ) = CSItemSection(id, title, "", items, store, isCollapsedDefault)
 
         fun <Item> dialogSection(
             title: String, vararg items: Item
