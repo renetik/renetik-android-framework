78ac446e82c13279db92ada276e18eb0b1de8fdf
Rene Dohan <dohan.rene@email.com>
2025-01-17 12:43:52 +0800

    ...

--- library/renetik-android-ui/src/main/java/renetik/android/ui/extensions/ViewGroup+CSViewInterface.kt
+++ library/renetik-android-ui/src/main/java/renetik/android/ui/extensions/ViewGroup+CSViewInterface.kt
@@ -3,9 +3,24 @@ package renetik.android.ui.extensions
 import android.view.ViewGroup
 import android.view.ViewGroup.LayoutParams
 import androidx.core.view.contains
+import renetik.android.core.kotlin.primitives.isTrue
+import renetik.android.event.registration.CSHasChangeValue
+import renetik.android.event.registration.action
 import renetik.android.ui.extensions.view.add
 import renetik.android.ui.extensions.view.remove
 import renetik.android.ui.protocol.CSViewInterface
+
+fun ViewGroup.addIf(
+    property: CSHasChangeValue<Boolean>, function: () -> CSViewInterface) {
+    var addedView: CSViewInterface? = null
+    property.action {
+        if (it.isTrue) addedView = add(function())
+        else {
+            addedView?.removeFromSuperview()
+            addedView = null
+        }
+    }
+}
 
 operator fun <View : CSViewInterface> ViewGroup.plusAssign(view: View) {
     add(view.view)
