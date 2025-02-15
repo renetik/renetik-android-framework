ced799665818d07a3e7eae83105f6c27c89c0af0
Rene Dohan <dohan.rene@email.com>
2025-02-15 21:22:48 +0800

    AutoCompleteTextView extensions..

--- library/renetik-android-ui/src/main/java/renetik/android/ui/extensions/widget/AutoCompleteTextView+.kt
+++ library/renetik-android-ui/src/main/java/renetik/android/ui/extensions/widget/AutoCompleteTextView+.kt
@@ -1,6 +1,30 @@
 package renetik.android.ui.extensions.widget
 
+import android.R.layout.simple_spinner_dropdown_item
+import android.widget.ArrayAdapter
 import android.widget.AutoCompleteTextView
 
 val AutoCompleteTextView.selectedIndex: Int?
     get() = if (listSelection >= 0) listSelection else null
+
+// simple_dropdown_item_1line
+fun AutoCompleteTextView.setDropDown(
+    string: Int, onItemSelected: (position: Int) -> Unit) = apply {
+    val categories = resources.getStringArray(string)
+    val adapter = ArrayAdapter(context, simple_spinner_dropdown_item, categories)
+    setAdapter(adapter)
+    setOnClickListener { showDropDown() }
+    setOnItemClickListener { _, _, position, _ ->
+        onItemSelected(position)
+    }
+    keyListener = null //To disable user editing
+}
+
+fun <T:AutoCompleteTextView> T.setDropDown(
+    strings: List<String>, onItemSelected: ((position: Int) -> Unit)? = null) = apply {
+    val adapter = ArrayAdapter(context, simple_spinner_dropdown_item, strings)
+    setAdapter(adapter)
+    setOnClickListener { showDropDown() }
+    setOnItemClickListener { _, _, position, _ -> onItemSelected?.invoke(position) }
+    keyListener = null //To disable user editing
+}
