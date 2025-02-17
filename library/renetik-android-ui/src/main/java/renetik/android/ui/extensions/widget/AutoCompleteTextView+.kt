b26bf018decdd1fc444b9f92c889d50b16956398
Rene Dohan <dohan.rene@email.com>
2025-02-17 22:12:10 +0800

    Return selection of items by typing in models and brand of forum tipic

--- library/renetik-android-ui/src/main/java/renetik/android/ui/extensions/widget/AutoCompleteTextView+.kt
+++ library/renetik-android-ui/src/main/java/renetik/android/ui/extensions/widget/AutoCompleteTextView+.kt
@@ -11,22 +11,22 @@ val AutoCompleteTextView.selectedIndex: Int?
 
 // simple_dropdown_item_1line
 fun AutoCompleteTextView.setDropDown(
-    string: Int, onItemSelected: (position: Int) -> Unit) = apply {
+    string: Int, disableEdit: Boolean = true,
+    onItemSelected: ((position: Int) -> Unit)? = null) = apply {
     val categories = resources.getStringArray(string)
     val adapter = ArrayAdapter(context, simple_spinner_dropdown_item, categories)
     setAdapter(adapter)
     setOnClickListener { showDropDown() }
-    setOnItemClickListener { _, _, position, _ ->
-        onItemSelected(position)
-    }
-    keyListener = null //To disable user editing
+    setOnItemClickListener { _, _, position, _ -> onItemSelected?.invoke(position) }
+    if (disableEdit) keyListener = null //To disable user editing
 }
 
 fun <T : AutoCompleteTextView> T.setDropDown(
-    strings: List<String>, onItemSelected: ((position: Int) -> Unit)? = null) = apply {
+    strings: List<String>, disableEdit: Boolean = true,
+    onItemSelected: ((position: Int) -> Unit)? = null) = apply {
     val adapter = ArrayAdapter(context, simple_spinner_dropdown_item, strings)
     setAdapter(adapter)
     setOnClickListener { showDropDown() }
     setOnItemClickListener { _, _, position, _ -> onItemSelected?.invoke(position) }
-    keyListener = null //To disable user editing
+    if (disableEdit) keyListener = null
 }
