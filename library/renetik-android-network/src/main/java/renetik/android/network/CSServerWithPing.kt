19be90b3aae8406be6be92f015f934a9428d763b
Rene Dohan <dohan.rene@email.com>
2025-03-08 11:35:44 +0800

    Remove process and operation..

--- library/renetik-android-network/src/main/java/renetik/android/network/CSServerWithPing.kt
+++
@@ -1,8 +0,0 @@
-package renetik.android.network
-
-import renetik.android.network.data.CSServerMapData
-import renetik.android.event.process.CSProcess
-
-interface CSServerWithPing {
-    fun ping(): CSProcess<CSServerMapData>
-}
--- library/renetik-android-network/src/main/java/renetik/android/network/operation/CSHttpOperation+.kt
+++
@@ -1,3 +0,0 @@
-package renetik.android.network.operation
-
-fun <Data : Any> CSHttpOperation<Data>.refresh() = apply { isRefresh = true }
