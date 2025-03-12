8cfcf85b30c2694fe79e0b37d62d57d41a2707cd
Rene Dohan <dohan.rene@gmail.com>
2025-03-12 21:18:45 +0800

    Fix new drop down restore state on orientation change

--- library/renetik-android-ui/src/main/java/renetik/android/ui/extensions/widget/AutoCompleteTextView+.kt
+++ library/renetik-android-ui/src/main/java/renetik/android/ui/extensions/widget/AutoCompleteTextView+.kt
@@ -3,6 +3,7 @@ package renetik.android.ui.extensions.widget
 import android.R.layout.simple_spinner_dropdown_item
 import android.widget.ArrayAdapter
 import android.widget.AutoCompleteTextView
+import android.widget.Filter
 
 fun AutoCompleteTextView.reset() = apply { setAdapter(null); text("") }
 
@@ -11,10 +12,19 @@ val AutoCompleteTextView.selectedIndex: Int?
 
 // simple_dropdown_item_1line
 fun AutoCompleteTextView.setDropDown(
-    string: Int, disableEdit: Boolean = true,
+    string: Int, disableEdit: Boolean = true, selectedIndex: Int? = null,
     onItemSelected: ((position: Int) -> Unit)? = null) = apply {
     val strings = resources.getStringArray(string)
-    val adapter = ArrayAdapter(context, simple_spinner_dropdown_item, strings)
+    val adapter = object : ArrayAdapter<String>(context, simple_spinner_dropdown_item, strings) {
+        override fun getFilter(): Filter = object : Filter() {
+            override fun performFiltering(constraint: CharSequence?): FilterResults =
+                FilterResults().apply { values = strings; count = strings.size }
+
+            override fun publishResults(constraint: CharSequence?, results: FilterResults?) =
+                notifyDataSetChanged()
+        }
+    }
+    selectedIndex?.let(strings::getOrNull)?.also { setText(it, false) }
     setAdapter(adapter)
     setOnClickListener { showDropDown() }
     setOnItemClickListener { _, _, position, _ ->
