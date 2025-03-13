fe6c5c7d8a240a9ba3dbfa9f8f114a19950682a0
Rene Dohan <dohan.rene@gmail.com>
2025-03-13 19:11:00 +0800

    New poi add dialog implementation

--- library/renetik-android-ui/src/main/java/renetik/android/ui/extensions/widget/AutoCompleteTextView+.kt
+++ library/renetik-android-ui/src/main/java/renetik/android/ui/extensions/widget/AutoCompleteTextView+.kt
@@ -12,9 +12,14 @@ val AutoCompleteTextView.selectedIndex: Int?
 
 // simple_dropdown_item_1line
 fun AutoCompleteTextView.setDropDown(
-    string: Int, disableEdit: Boolean = true, selectedIndex: Int? = null,
-    onItemSelected: ((position: Int) -> Unit)? = null) = apply {
-    val strings = resources.getStringArray(string)
+    stringArray: Int, disableEdit: Boolean = true, selectedIndex: Int? = null,
+    onItemSelected: ((position: Int) -> Unit)? = null) =
+    setDropDown(resources.getStringArray(stringArray).toList(),
+        disableEdit, selectedIndex, onItemSelected)
+
+fun <T : AutoCompleteTextView> T.setDropDown(
+    strings: List<String>, disableEdit: Boolean = true,
+    selectedIndex: Int? = null, onItemSelected: ((position: Int) -> Unit)? = null) = apply {
     val adapter = object : ArrayAdapter<String>(context, simple_spinner_dropdown_item, strings) {
         override fun getFilter(): Filter = object : Filter() {
             override fun performFiltering(constraint: CharSequence?): FilterResults =
@@ -32,17 +37,4 @@ fun AutoCompleteTextView.setDropDown(
         onItemSelected?.invoke(strings.indexOf(selectedItem))
     }
     if (disableEdit) keyListener = null //To disable user editing
-}
-
-fun <T : AutoCompleteTextView> T.setDropDown(
-    strings: List<String>, disableEdit: Boolean = true,
-    onItemSelected: ((position: Int) -> Unit)? = null) = apply {
-    val adapter = ArrayAdapter(context, simple_spinner_dropdown_item, strings)
-    setAdapter(adapter)
-    setOnClickListener { showDropDown() }
-    setOnItemClickListener { _, _, position, _ ->
-        val selectedItem = adapter.getItem(position) as String
-        onItemSelected?.invoke(strings.indexOf(selectedItem))
-    }
-    if (disableEdit) keyListener = null
 }
