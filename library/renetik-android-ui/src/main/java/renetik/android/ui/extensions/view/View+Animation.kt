f5e24b0dd48bdd7841bfc09f219933a51eedc310
Rene Dohan <dohan.rene@gmail.com>
2025-03-19 15:27:49 +0800

    ...

--- library/renetik-android-ui/src/main/java/renetik/android/ui/extensions/view/View+Animation.kt
+++ library/renetik-android-ui/src/main/java/renetik/android/ui/extensions/view/View+Animation.kt
@@ -4,6 +4,7 @@ import android.view.View
 import android.view.ViewPropertyAnimator
 import renetik.android.core.lang.CSEnvironment.app
 import renetik.android.core.lang.CSLeakCanary
+import renetik.android.event.common.CSHasDestruct
 
 val shortAnimationDuration =
     app.resources.getInteger(android.R.integer.config_shortAnimTime)
@@ -30,7 +31,21 @@ fun <T : View> T.fadeIn(duration: Int = fadeAnimationDuration): ViewPropertyAnim
 fun <T : View> T.fadeOut(
     duration: Int = fadeAnimationDuration,
     invisible: Boolean = false,
-    onDone: (() -> Unit)? = null,
+) = fadeOut(duration, invisible, onDone = { })
+
+fun <T : View> T.fadeOut(
+    parent: CSHasDestruct,
+    duration: Int = fadeAnimationDuration,
+    invisible: Boolean = false,
+    onDone: (() -> Unit)
+) = fadeOut(duration, invisible, onDone = {
+    if (!parent.isDestructed) onDone()
+})
+
+private fun <T : View> T.fadeOut(
+    duration: Int = fadeAnimationDuration,
+    invisible: Boolean = false,
+    onDone: (() -> Unit)
 ): ViewPropertyAnimator? = when {
     CSLeakCanary.isEnabled -> {
         if (invisible) invisible() else gone()
