ab50ed41af66835c91beb5e71a7348bed1f7e0f2
Rene Dohan <dohan.rene@gmail.com>
2025-04-09 13:10:44 +0800

    Refactor: Improve CSTouchEventArgs and processTouchEvent
    - Remove unnecessary companion object from CSTouchEventArgs.
    - Change CSTouchEventArgs.handled to set isHandled to true.
    - Refactor processTouchEvent to use CSTouchEventArgs constructor.
    - Remove unnecessary isHandled from processTouchEvent, using CSTouchEventArgs.isHandled.

--- library/renetik-android-ui/src/main/java/renetik/android/ui/view/CSHasTouchEvent.kt
+++ library/renetik-android-ui/src/main/java/renetik/android/ui/view/CSHasTouchEvent.kt
@@ -3,22 +3,14 @@ package renetik.android.ui.view
 import android.graphics.Canvas
 import android.view.MotionEvent
 import android.view.View
+import renetik.android.core.kotlin.then
 import renetik.android.event.CSEvent
 import renetik.android.event.registration.CSRegistration
 
-data class CSTouchEventArgs(var event: MotionEvent, var isHandled: Boolean) {
-    fun handled() {
-        isHandled = true
-    }
-
+data class CSTouchEventArgs(val event: MotionEvent, var isHandled: Boolean) {
+    fun handled() = then { isHandled = true }
     val action get() = event.action
     val actionMasked get() = event.actionMasked
-
-    companion object {
-        private val instance = CSTouchEventArgs(MotionEvent.obtain(0, 0, 0, 0f, 0f, 0), false)
-        fun with(event: MotionEvent) = instance.apply { this.event = event }
-        val isHandled: Boolean get() = instance.isHandled
-    }
 }
 
 interface CSHasTouchEvent {
@@ -26,10 +18,12 @@ interface CSHasTouchEvent {
     val eventOnTouch: CSEvent<CSTouchEventArgs>
 
     fun processTouchEvent(event: MotionEvent): Boolean {
-        if (eventOnTouch.isListened)
-            eventOnTouch.fire(CSTouchEventArgs.with(event))
-        val handled = eventOnTouch.isListened && CSTouchEventArgs.isHandled
-        return handled
+        if (eventOnTouch.isListened) {
+            val args = CSTouchEventArgs(event, false)
+            eventOnTouch.fire(args)
+            return args.isHandled
+        }
+        return false
     }
 }
 
