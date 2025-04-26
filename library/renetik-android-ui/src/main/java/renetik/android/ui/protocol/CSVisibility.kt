deecaced998cfedcde51752f5c6f0ba8bdaa8458
Rene Dohan <dohan.rene@gmail.com>
2025-04-26 13:21:17 +0800

    Refactor CSVisibility protocol and add isVisible property

--- library/renetik-android-ui/src/main/java/renetik/android/ui/protocol/CSVisibility.kt
+++ library/renetik-android-ui/src/main/java/renetik/android/ui/protocol/CSVisibility.kt
@@ -3,6 +3,7 @@ package renetik.android.ui.protocol
 import renetik.android.event.registration.CSHasChangeValue
 
 interface CSVisibility {
+    //TODO: Rename to isVisibility to avoid platform clash
     val isVisible: CSHasChangeValue<Boolean>
     fun updateVisibility() = Unit
 }
