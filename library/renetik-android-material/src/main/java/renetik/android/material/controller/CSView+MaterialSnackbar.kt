f5f1fff8cc01336e3975096faf3b45fc4ebea5c0
Rene Dohan <dohan.rene@gmail.com>
2025-05-28 16:18:23 +0800

    Refactor: Rename snackBar to snackbar for consistency and add Duration parameter
    This commit refactors the `snackBar` extension functions for `View` and `CSView` to `snackbar` to maintain consistency with Android Material Design naming conventions.
    
    Additionally, a `Duration` parameter has been added to these functions, allowing for more precise control over the snackbar's display time. If no duration is provided, a default of 5 seconds is used.
    
    The `displayMessage` function in `UIDevice+` has been removed as it was no longer necessary after these changes.
    
    Key changes:
    - Renamed `snackBar` to `snackbar` in `View+CSMaterialSnackbar.kt` and `CSView+MaterialSnackbar.kt`.
    - Added a `time: Duration?` parameter to `snackbar` functions.
    - If the snackbar is shown in a `FrameLayout` without an action, it will be centered at the bottom with wrap_content width.
    - The snackbar will now use a `CSLater` to dismiss itself after the specified duration if no action is present.
    - Removed `displayMessage` function from `UIDevice+.kt`.

--- library/renetik-android-material/src/main/java/renetik/android/material/controller/CSView+MaterialSnackbar.kt
+++ library/renetik-android-material/src/main/java/renetik/android/material/controller/CSView+MaterialSnackbar.kt
@@ -2,18 +2,27 @@ package renetik.android.material.controller
 
 import renetik.android.controller.base.CSView
 import renetik.android.core.lang.CSButtonAction
-import renetik.android.material.extensions.snackBarError
-import renetik.android.material.extensions.snackBarInfo
-import renetik.android.material.extensions.snackBarWarn
+import renetik.android.material.extensions.snackbarError
+import renetik.android.material.extensions.snackbarInfo
+import renetik.android.material.extensions.snackbarWarn
+import kotlin.time.Duration
 
-fun CSView<*>.snackBarWarn(text: String, action: CSButtonAction? = null) =
-    view.snackBarWarn(text, action)
+fun CSView<*>.snackBarWarn(
+    text: String, time: Duration? = null,
+    action: CSButtonAction? = null
+) = view.snackbarWarn(this, text, time, action)
 
-fun CSView<*>.snackBarError(text: String, title: String, onClick: () -> Unit) =
-    view.snackBarError(text, CSButtonAction(title, onClick))
+fun CSView<*>.snackBarError(
+    text: String, title: String,
+    time: Duration? = null, onClick: () -> Unit
+) = view.snackbarError(this, text, time, CSButtonAction(title, onClick))
 
-fun CSView<*>.snackBarError(text: String, action: CSButtonAction? = null) =
-    view.snackBarError(text, action)
+fun CSView<*>.snackBarError(
+    text: String, time: Duration? = null,
+    action: CSButtonAction? = null
+) = view.snackbarError(this, text, time, action)
 
-fun CSView<*>.snackBarInfo(text: String, action: CSButtonAction? = null) =
-    view.snackBarInfo(text, action)
+fun CSView<*>.snackBarInfo(
+    text: String, time: Duration? = null,
+    action: CSButtonAction? = null
+) = view.snackbarInfo(this, text, time, action)
--- library/renetik-android-material/src/main/java/renetik/android/material/extensions/View+CSMaterialSnackbar.kt
+++ library/renetik-android-material/src/main/java/renetik/android/material/extensions/View+CSMaterialSnackbar.kt
@@ -1,53 +1,90 @@
 package renetik.android.material.extensions
 
+import android.view.Gravity.BOTTOM
+import android.view.Gravity.CENTER
 import android.view.View
+import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
+import android.widget.FrameLayout
 import androidx.annotation.ColorInt
 import androidx.appcompat.R.attr.colorError
+import androidx.core.view.updateLayoutParams
 import com.google.android.material.R.attr.colorOnError
 import com.google.android.material.R.attr.colorPrimary
 import com.google.android.material.R.attr.colorSurface
 import com.google.android.material.snackbar.BaseTransientBottomBar.LENGTH_INDEFINITE
-import com.google.android.material.snackbar.BaseTransientBottomBar.LENGTH_LONG
 import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.snackbar.Snackbar.Callback
 import com.google.android.material.snackbar.Snackbar.make
 import renetik.android.core.extensions.content.attributeColor
 import renetik.android.core.lang.CSButtonAction
+import renetik.android.core.lang.Func
+import renetik.android.event.registration.CSHasRegistrations
+import renetik.android.event.util.CSLater.later
+import renetik.android.ui.extensions.view.removeFromSuperview
+import renetik.android.ui.extensions.widget.layoutMatch
+import kotlin.time.Duration
+import kotlin.time.Duration.Companion.seconds
 
-fun View.snackBar(title: String) = snackBar(title, time = LENGTH_LONG)
+fun View.snackbarWarn(
+    parent: CSHasRegistrations, title: String,
+    time: Duration? = null, action: CSButtonAction? = null
+) = snackbar(parent, title, backColor = context.attributeColor(colorError),
+    textColor = context.attributeColor(colorOnError), action = action, time)
 
-fun View.snackBar(
-    title: String, @ColorInt backColor: Int? = null, @ColorInt textColor: Int? = null,
-    time: Int = LENGTH_LONG, action: CSButtonAction? = null
-): Snackbar {
-    val bar = make(this, title, if (action != null) LENGTH_INDEFINITE else time)
-    action?.let {
-        bar.setAction(action.title) {
-            action.onClick()
-            bar.dismiss()
+fun View.snackbarError(
+    parent: CSHasRegistrations, title: String,
+    time: Duration? = null, action: CSButtonAction? = null
+) = snackbar(parent, title, backColor = context.attributeColor(colorError),
+    textColor = context.attributeColor(colorOnError), action = action, time)
+
+fun View.snackbarInfo(
+    parent: CSHasRegistrations, title: String,
+    time: Duration? = null, action: CSButtonAction? = null
+) = snackbar(parent, title, backColor = context.attributeColor(colorSurface),
+    textColor = context.attributeColor(colorPrimary), action = action, time)
+
+fun View.snackbar(parent: CSHasRegistrations, title: String) =
+    snackbar(parent, title, time = 5.seconds)
+
+fun View.snackbar(
+    parent: CSHasRegistrations,
+    title: String,
+    @ColorInt backColor: Int? = null,
+    @ColorInt textColor: Int? = null,
+    action: CSButtonAction? = null,
+    time: Duration? = null
+) {
+    (this as? FrameLayout)?.also {
+        FrameLayout(context).apply {
+            id = android.R.id.content
+            it.addView(this, layoutMatch)
+            showBar(parent, title, backColor, textColor, time,
+                action, onDismiss = { removeFromSuperview() }
+            ).view.updateLayoutParams<FrameLayout.LayoutParams> {
+                gravity = CENTER or BOTTOM; width = WRAP_CONTENT
+            }
         }
-    }
-    backColor?.let { bar.view.setBackgroundColor(it) }
-    textColor?.let { bar.setTextColor(it) }
-    textColor?.let { bar.setTextMaxLines(Int.MAX_VALUE) }
-    textColor?.let { bar.setActionTextColor(it) }
-    bar.show()
-    return bar
+    } ?: showBar(parent, title, backColor, textColor, time, action)
 }
 
-fun View.snackBarWarn(title: String, action: CSButtonAction? = null) =
-    snackBar(
-        title, backColor = context.attributeColor(colorError),
-        textColor = context.attributeColor(colorOnError), action = action
-    )
-
-fun View.snackBarError(title: String, action: CSButtonAction? = null) =
-    snackBar(
-        title, backColor = context.attributeColor(colorError),
-        textColor = context.attributeColor(colorOnError), action = action
-    )
-
-fun View.snackBarInfo(title: String, action: CSButtonAction? = null) =
-    snackBar(
-        title, backColor = context.attributeColor(colorSurface),
-        textColor = context.attributeColor(colorPrimary), action = action
-    )
+private fun View.showBar(
+    parent: CSHasRegistrations,
+    title: String, @ColorInt backColor: Int? = null, @ColorInt textColor: Int? = null,
+    time: Duration?, action: CSButtonAction? = null, onDismiss: Func? = null
+): Snackbar {
+    val snackbar = make(this, title, LENGTH_INDEFINITE)
+    action?.also { snackbar.setAction(action.title) { action.onClick(); snackbar.dismiss() } }
+    val dismissTimer = if (action == null)
+        parent.later(after = time ?: 5.seconds) { snackbar.dismiss() } else null
+    snackbar.addCallback(object : Callback() {
+        override fun onDismissed(transientBottomBar: Snackbar?, event: Int) {
+            dismissTimer?.cancel(); onDismiss?.invoke()
+        }
+    })
+    backColor?.let { snackbar.view.setBackgroundColor(it) }
+    textColor?.let { snackbar.setTextColor(it) }
+    textColor?.let { snackbar.setTextMaxLines(Int.MAX_VALUE) }
+    textColor?.let { snackbar.setActionTextColor(it) }
+    snackbar.show()
+    return snackbar
+}
