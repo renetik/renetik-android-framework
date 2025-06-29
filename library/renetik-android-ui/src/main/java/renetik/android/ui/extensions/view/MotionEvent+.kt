93fe5340d56430026fcefa1d236ea514bf54a589
Rene Dohan <dohan.rene@gmail.com>
2025-06-29 15:41:24 +0800

    ...

--- library/renetik-android-ui/src/main/java/renetik/android/ui/extensions/view/MotionEvent+.kt
+++ library/renetik-android-ui/src/main/java/renetik/android/ui/extensions/view/MotionEvent+.kt
@@ -2,10 +2,15 @@ package renetik.android.ui.extensions.view
 
 import android.view.MotionEvent
 import android.view.View
+import renetik.android.core.math.CSPoint
 
 fun MotionEvent.isInside(view: View) =
     (view.x <= x && x < view.width) && (view.y <= y && x < view.height)
 
-fun MotionEvent.offsetLocation(location: IntArray) {
+fun MotionEvent.offsetLocation(location: IntArray) =
     offsetLocation(location[0].toFloat(), location[1].toFloat())
-}
+
+fun MotionEvent.clampedPoint(view: View): CSPoint<Float> = CSPoint(
+    x.coerceIn(0f, view.width.toFloat()),
+    y.coerceIn(0f, view.height.toFloat())
+)
