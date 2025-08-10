561804ee14435cd6c5bdebdec71c65fc08dd1815
Rene Dohan <dohan.rene@gmail.com>
2025-08-11 00:39:37 +0800

    Fix: Prevent InputMethodManager leak in CSActivity
    This commit addresses a potential memory leak related to the `InputMethodManager` in `CSActivity`.
    
    The `fixInputMethodManagerLeakSafe()` extension function is now called within the `onDestruct()` method of `CSActivity`. This ensures that any references held by the `InputMethodManager` are cleared when the activity is being destroyed, preventing a common source of memory leaks in Android applications.

--- library/renetik-android-controller/src/main/java/renetik/android/controller/base/CSActivity.kt
+++ library/renetik-android-controller/src/main/java/renetik/android/controller/base/CSActivity.kt
@@ -15,6 +15,7 @@ import renetik.android.event.fire
 import renetik.android.event.property.CSProperty.Companion.property
 import renetik.android.event.registration.CSRegistrationsMap
 import renetik.android.ui.extensions.clearContentView
+import renetik.android.ui.extensions.fixInputMethodManagerLeakSafe
 import renetik.android.ui.protocol.CSVisibility
 
 abstract class CSActivity<ActivityView : CSActivityView<out ViewGroup>> : AppCompatActivity(),
@@ -109,6 +110,7 @@ abstract class CSActivity<ActivityView : CSActivityView<out ViewGroup>> : AppCom
         eventDestruct.fire().clear()
         activityView = null
         logInfo()
+        fixInputMethodManagerLeakSafe()
     }
 
     public override fun onDestroy() {
