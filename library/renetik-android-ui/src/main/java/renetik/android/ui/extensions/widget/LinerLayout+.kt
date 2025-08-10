bc102c1994c15f8a44778a1b61382e211c31946b
Rene Dohan <dohan.rene@gmail.com>
2025-08-10 12:59:00 +0800

    feat: Add optional property and update LinearLayout extensions
    This commit introduces an `optional` extension property for `CSProperty` and refactors `verticalLayout` and `horizontalLayout` extension functions.
    
    - Added an `optional` extension property to `CSProperty<T>` that returns a `CSProperty<T?>`. This allows treating a non-nullable property as nullable.
    - The `verticalLayout` and `horizontalLayout` extension functions for `Context` have been changed to be extensions of `CSViewInterface`. This provides a more consistent API within the `renetik-android-ui` library.

--- library/renetik-android-ui/src/main/java/renetik/android/ui/extensions/widget/LinerLayout+.kt
+++ library/renetik-android-ui/src/main/java/renetik/android/ui/extensions/widget/LinerLayout+.kt
@@ -2,6 +2,7 @@ package renetik.android.ui.extensions.widget
 
 import android.content.Context
 import android.widget.LinearLayout
+import renetik.android.ui.protocol.CSViewInterface
 
 fun HorizontalLayout(context: Context) = LinearLayout(context).also {
     it.orientation = LinearLayout.HORIZONTAL
@@ -11,10 +12,10 @@ fun VerticalLayout(context: Context) = LinearLayout(context).also {
     it.orientation = LinearLayout.VERTICAL
 }
 
-fun Context.verticalLayout(init: (LinearLayout).() -> Unit) =
-    VerticalLayout(this).apply { init(this) }
+fun CSViewInterface.verticalLayout(init: (LinearLayout).() -> Unit) =
+    VerticalLayout(context).apply { init(this) }
 
 
-fun Context.horizontalLayout(init: (LinearLayout).() -> Unit) =
-    HorizontalLayout(this).apply { init(this) }
+fun CSViewInterface.horizontalLayout(init: (LinearLayout).() -> Unit) =
+    HorizontalLayout(context).apply { init(this) }
 
