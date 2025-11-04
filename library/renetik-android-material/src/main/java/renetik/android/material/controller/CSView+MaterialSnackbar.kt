349366e6d58a1dce598937ba2a1cc93a88d436f7
Rene Dohan <dohan.rene@gmail.com>
2025-11-04 23:07:13 +0100

    refactor(material): Ensure snackbar functions run on main thread
    This commit wraps the `snackBarWarn`, `snackBarError`, and `snackBarInfo` extension functions for `CSView` with `onMain`. This ensures that all snackbar presentations are safely executed on the main thread, preventing potential threading issues.

--- library/renetik-android-material/src/main/java/renetik/android/material/controller/CSView+MaterialSnackbar.kt
+++ library/renetik-android-material/src/main/java/renetik/android/material/controller/CSView+MaterialSnackbar.kt
@@ -2,6 +2,7 @@ package renetik.android.material.controller
 
 import renetik.android.controller.base.CSView
 import renetik.android.core.lang.CSButtonAction
+import renetik.android.event.util.CSLater.onMain
 import renetik.android.material.extensions.snackbarError
 import renetik.android.material.extensions.snackbarInfo
 import renetik.android.material.extensions.snackbarWarn
@@ -10,19 +11,19 @@ import kotlin.time.Duration
 fun CSView<*>.snackBarWarn(
     text: String, time: Duration? = null,
     action: CSButtonAction? = null
-) = view.snackbarWarn(this, text, time, action)
+) = onMain { view.snackbarWarn(this, text, time, action) }
 
 fun CSView<*>.snackBarError(
     text: String, title: String,
     time: Duration? = null, onClick: () -> Unit
-) = view.snackbarError(this, text, time, CSButtonAction(title, onClick))
+) = onMain { view.snackbarError(this, text, time, CSButtonAction(title, onClick)) }
 
 fun CSView<*>.snackBarError(
     text: String, time: Duration? = null,
     action: CSButtonAction? = null
-) = view.snackbarError(this, text, time, action)
+) = onMain { view.snackbarError(this, text, time, action) }
 
 fun CSView<*>.snackBarInfo(
     text: String, time: Duration? = null,
     action: CSButtonAction? = null
-) = view.snackbarInfo(this, text, time, action)
+) = onMain { view.snackbarInfo(this, text, time, action) }
