c97cf7efb5b382ccd4096f9a36f7e8e7a737fc4b
Rene Dohan <dohan.rene@gmail.com>
2025-11-05 12:09:31 +0100

    refactor: Remove deprecated `startActivityForResult` helpers
    This commit removes deprecated helper functions and the `CSStartActivityResult` enum related to `startActivityForResult` in `CSActivityView`.
    
    These functions were previously marked for deprecation and have now been removed to clean up the codebase.

--- library/renetik-android-controller/src/main/java/renetik/android/controller/extensions/CSActivityView+StartActivity.kt
+++ library/renetik-android-controller/src/main/java/renetik/android/controller/extensions/CSActivityView+StartActivity.kt
@@ -1,49 +1,41 @@
 package renetik.android.controller.extensions
 
-import android.content.ActivityNotFoundException
 import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import androidx.core.net.toUri
-import renetik.android.controller.base.CSActivityView
-import renetik.android.controller.extensions.CSStartActivityResult.ActivityNotFound
-import renetik.android.controller.extensions.CSStartActivityResult.Cancel
-import renetik.android.core.kotlin.primitives.random
-import renetik.android.event.registration.CSRegistration
-import renetik.android.event.registration.plus
 
-@Deprecated("DEPRECATION")
-fun CSActivityView<*>.startActivityForResult(
-    activityClass: Class<out AppCompatActivity>, requestCode: Int
-) = startActivityForResult(Intent(activity(), activityClass), requestCode)
-
-@Deprecated("DEPRECATION")
-enum class CSStartActivityResult { Cancel, ActivityNotFound }
-
-@Deprecated("DEPRECATION")
-fun CSActivityView<*>.startActivityForResult(
-    intent: Intent, onSuccess: (Intent?) -> Unit,
-    onFailure: ((CSStartActivityResult) -> Unit)? = null
-) {
-    try {
-        val requestCode = Int.random(0, 9999)
-        startActivityForResult(intent, requestCode)
-        var registration: CSRegistration? = null
-        registration = this + activity().onActivityResult.listen { result ->
-            if (result.requestCode == requestCode) {
-                if (result.isOK()) onSuccess(result.data)
-                else onFailure?.invoke(Cancel)
-                registration?.cancel()
-            }
-        }
-    } catch (ex: ActivityNotFoundException) {
-        onFailure?.invoke(ActivityNotFound)
-    }
-}
-
-@Deprecated("DEPRECATION")
-fun CSActivityView<*>.startActivityForResult(intent: Intent, requestCode: Int) =
-    activity().startActivityForResult(intent, requestCode)
+//@Deprecated("DEPRECATION")
+//fun CSActivityView<*>.startActivityForResult(
+//    activityClass: Class<out AppCompatActivity>, requestCode: Int
+//) = startActivityForResult(Intent(activity(), activityClass), requestCode)
+//
+//@Deprecated("DEPRECATION")
+//enum class CSStartActivityResult { Cancel, ActivityNotFound }
+//
+//@Deprecated("DEPRECATION")
+//fun CSActivityView<*>.startActivityForResult(
+//    intent: Intent, onSuccess: (Intent?) -> Unit,
+//    onFailure: ((CSStartActivityResult) -> Unit)? = null
+//) {
+//    try {
+//        val requestCode = Int.random(0, 9999)
+//        startActivityForResult(intent, requestCode)
+//        var registration: CSRegistration? = null
+//        registration = this + activity().onActivityResult.listen { result ->
+//            if (result.requestCode == requestCode) {
+//                if (result.isOK()) onSuccess(result.data)
+//                else onFailure?.invoke(Cancel)
+//                registration?.cancel()
+//            }
+//        }
+//    } catch (ex: ActivityNotFoundException) {
+//        onFailure?.invoke(ActivityNotFound)
+//    }
+//}
+//
+//@Deprecated("DEPRECATION")
+//fun CSActivityView<*>.startActivityForResult(intent: Intent, requestCode: Int) =
+//    activity().startActivityForResult(intent, requestCode)
 
 fun Context.openUri(uri: String, appPackage: String? = null): Boolean =
     Intent(Intent.ACTION_VIEW, uri.toUri()).apply {
