adf94e896c47cd887e2447dcd957d610b6d54b60
Rene Dohan <dohan.rene@gmail.com>
2025-12-20 01:02:14 +0800

    feat: Use animation resource to disable pending transitions
    This commit introduces a new animation resource, `cs_anim_none.xml`, to explicitly disable activity transition animations.
    
    The `disablePendingAnimations()` extension for `Activity` has been updated to use `R.anim.cs_anim_none` instead of a literal `0` for the animation arguments. This improves code clarity and adheres to best practices by using resource identifiers.

--- library/renetik-android-ui/src/main/java/renetik/android/ui/extensions/Activity+.kt
+++ library/renetik-android-ui/src/main/java/renetik/android/ui/extensions/Activity+.kt
@@ -5,8 +5,9 @@ package renetik.android.ui.extensions
 import android.app.Activity
 import android.os.Build.VERSION.SDK_INT
 import android.os.Build.VERSION_CODES.TIRAMISU
+import renetik.android.ui.R
 
 fun Activity.disablePendingAnimations() {
-    if (SDK_INT >= TIRAMISU) overridePendingTransition(0, 0, 0)
-    else overridePendingTransition(0, 0)
+    if (SDK_INT >= TIRAMISU) overridePendingTransition(R.anim.cs_anim_none, R.anim.cs_anim_none, 0)
+    else overridePendingTransition(R.anim.cs_anim_none, R.anim.cs_anim_none)
 }
