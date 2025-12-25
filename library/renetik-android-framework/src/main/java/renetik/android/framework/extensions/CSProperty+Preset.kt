48f65c689f2bf822a81df779b8d0f8d93c7112d7
Rene Dohan <dohan.rene@gmail.com>
2025-12-26 00:23:45 +0800

    refactor: Simplify PanelSplitControllerView using new viewFactory
    This commit introduces a new, more generic `viewFactory` extension function for `List<Model>` that works with `CSHasRegistrations`. This new factory simplifies view creation and management.
    
    - **`CSProperty+Preset.kt`**: Added a new `viewFactory` overload that takes `CSHasRegistrations` as a parent, removing the dependency on `CSView`.
    - **`PanelSplitControllerView.kt`**: Refactored to use the new `viewFactory`, resulting in a more concise implementation for creating and managing track views. The explicit list `tracks` and its factory initialization have been removed.

--- library/renetik-android-framework/src/main/java/renetik/android/framework/extensions/CSProperty+Preset.kt
+++ library/renetik-android-framework/src/main/java/renetik/android/framework/extensions/CSProperty+Preset.kt
@@ -6,8 +6,10 @@ import android.view.ViewGroup.LayoutParams
 import renetik.android.controller.base.CSView
 import renetik.android.core.kotlin.collections.put
 import renetik.android.core.kotlin.primitives.update
+import renetik.android.event.common.CSHasRegistrationsHasDestruct
 import renetik.android.event.registration.CSHasChange
 import renetik.android.event.registration.CSHasChangeValue
+import renetik.android.event.registration.CSHasRegistrations
 import renetik.android.event.registration.CSRegistration
 import renetik.android.event.registration.invoke
 import renetik.android.event.registration.plus
@@ -53,6 +55,24 @@ fun <View : CSViewInterface, Model> MutableList<View>.viewFactory(
         }
     }
     list.forEach(::createView)
+    parent + eventAdded.onChange(::createView)
+}
+
+
+fun <View : CSViewInterface, Model> List<Model>.viewFactory(
+    parent: CSHasRegistrations, eventAdded: CSHasChange<Model>,
+    content: ViewGroup, layoutParams: LayoutParams? = null,
+    fromStart: Boolean = false, create: (Model) -> View
+) {
+    fun createView(model: Model) {
+        create(model).also { view ->
+            val addIndex = if (fromStart) 0 else -1
+            layoutParams?.let { content.add(view = view, params = it, addIndex) }
+                ?: content.add(view = view, addIndex)
+            view.eventDestruct { content -= view }
+        }
+    }
+    forEach(::createView)
     parent + eventAdded.onChange(::createView)
 }
 
