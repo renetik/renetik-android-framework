979b47207ae50cbdd005911918dc4310c7b190f4
Rene Dohan <dohan.rene@gmail.com>
2026-01-07 00:25:10 +0800

    feat: Add `forEachIndexedReverse` and inline other `ViewGroup` extensions
    This commit introduces a new `forEachIndexedReverse` extension function for `ViewGroup` to iterate over its children in reverse order.
    
    Additionally, the existing extensions `childAt` and `subViews` have been inlined for minor performance improvement. Unused imports were also removed.

--- library/renetik-android-ui/src/main/java/renetik/android/ui/extensions/view/ViewGroup+.kt
+++ library/renetik-android-ui/src/main/java/renetik/android/ui/extensions/view/ViewGroup+.kt
@@ -4,11 +4,10 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.annotation.LayoutRes
 import androidx.core.view.children
+import androidx.core.view.isEmpty
 import androidx.core.view.isNotEmpty
-import androidx.core.view.size
 import renetik.android.ui.extensions.inflate
 import renetik.android.ui.protocol.CSHasParentView
-import androidx.core.view.isEmpty
 
 operator fun ViewGroup.get(index: Int): View = getChildAt(index)
 
@@ -72,8 +71,14 @@ fun <T : ViewGroup> T.clear() = apply {
     removeAllViews()
 }
 
-fun ViewGroup.childAt(condition: (View) -> Boolean) = children.find(condition)
+inline fun ViewGroup.childAt(condition: (View) -> Boolean) = children.find(condition)
 
 fun ViewGroup.removeViews(from: Int) = removeViews(from, childCount - from)
 
-val ViewGroup.subViews: List<View> get() = children.toList()
+inline val ViewGroup.subViews: List<View> get() = children.toList()
+
+inline fun ViewGroup.forEachIndexedReverse(action: (index: Int, view: View) -> Unit) {
+    for (index in childCount - 1 downTo 0) {
+        action(index, getChildAt(index))
+    }
+}
