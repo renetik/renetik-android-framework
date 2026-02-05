a4d09427f8a90d9aae8b2a34aeed8b2152f83369
Rene Dohan <dohan.rene@gmail.com>
2026-02-05 12:18:48 +0800

    Update `CSStore` empty instance property naming
    * Rename `CSStore.EmptyStore` to `CSStore.Empty`.
    * Update `CSItemSection` to use the renamed `CSStore.Empty` property.

--- library/renetik-android-controller/src/main/java/renetik/android/controller/view/grid/item/CSItemSection.kt
+++ library/renetik-android-controller/src/main/java/renetik/android/controller/view/grid/item/CSItemSection.kt
@@ -1,7 +1,6 @@
 package renetik.android.controller.view.grid.item
 
 import renetik.android.store.CSStore
-import renetik.android.store.CSStore.Companion.EmptyStore
 import renetik.android.store.CSStore.Companion.runtimeStore
 import renetik.android.store.extensions.property
 
@@ -10,7 +9,7 @@ data class CSItemSection<Item>(
     val title: String,
     val description: String = "",
     var items: List<Item> = emptyList(),
-    val store: CSStore = EmptyStore,
+    val store: CSStore = CSStore.Empty,
     val isCollapsedDefault: Boolean = false
 ) {
     val isCollapsed = store.property(id, isCollapsedDefault)
