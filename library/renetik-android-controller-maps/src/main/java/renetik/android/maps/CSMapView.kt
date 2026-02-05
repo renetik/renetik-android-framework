734835481d0e9d0c8c24614ff4038b3d17841c51
Rene Dohan <dohan.rene@gmail.com>
2026-02-05 12:23:34 +0800

    Summarize the error handling logic in CSMapView
    * Remove "TODO" prefix from a comment in the `onCreate` error handling block.

--- library/renetik-android-controller-maps/src/main/java/renetik/android/maps/CSMapView.kt
+++ library/renetik-android-controller-maps/src/main/java/renetik/android/maps/CSMapView.kt
@@ -49,7 +49,7 @@ open class CSMapView(parent: CSActivityView<*>, private val options: GoogleMapOp
         } catch (ex: RuntimeException) {
             logError(ex)
             if (ex.message?.contains("API key not found") == true) unfinished()
-            //TODO finish if CSMapView is used ever
+            // finish if CSMapView is used ever
 //                dialog("Error", ex.message!!).cancelable(false).show {
 //                    exitProcess(1)
 //                }
