8104b59333d2b1c2538aeab0f9e806ff0e7e1ff2
Rene Dohan <dohan.rene@gmail.com>
2026-02-19 17:05:28 +0800

    Rename `CSButtonAction` to `CSAction` and update related snackbar and notification APIs
    * Rename `CSButtonAction` class to `CSAction` in `renetik-android-core`.
    * Update `PopupNotificationManager` and `MainView` to use `CSAction` for notification callbacks.
    * Refactor `View` and `CSView` snackbar extension methods (e.g., `snackWarn`, `snackError`, `snackInfo`) to accept `CSAction` instead of `CSButtonAction`.
    * Update `BluetoothMidiEnabler` to use the renamed `CSAction` for permission and location failure dialogs.
    * Update `PianoHighlight.registerCheck` to use `CSActivityView` as a parent and fix `FeatureStoreView` callsite to use the parent controller.

--- library/renetik-android-material/src/main/java/renetik/android/material/controller/CSView+MaterialSnackbar.kt
+++ library/renetik-android-material/src/main/java/renetik/android/material/controller/CSView+MaterialSnackbar.kt
@@ -1,7 +1,7 @@
 package renetik.android.material.controller
 
 import renetik.android.controller.base.CSView
-import renetik.android.core.lang.CSButtonAction
+import renetik.android.core.lang.CSAction
 import renetik.android.event.common.onMain
 import renetik.android.material.extensions.snackError
 import renetik.android.material.extensions.snackInfo
@@ -10,20 +10,20 @@ import kotlin.time.Duration
 
 fun CSView<*>.snackWarn(
     text: String, time: Duration? = null,
-    action: CSButtonAction? = null
+    action: CSAction? = null
 ) = onMain { view.snackWarn(this, text, time, action) }
 
 fun CSView<*>.snackError(
     text: String, title: String,
     time: Duration? = null, onClick: () -> Unit
-) = onMain { view.snackError(this, text, time, CSButtonAction(title, onClick)) }
+) = onMain { view.snackError(this, text, time, CSAction(title, onClick)) }
 
 fun CSView<*>.snackError(
     text: String, time: Duration? = null,
-    action: CSButtonAction? = null
+    action: CSAction? = null
 ) = onMain { view.snackError(this, text, time, action) }
 
 fun CSView<*>.snackInfo(
     text: String, time: Duration? = null,
-    action: CSButtonAction? = null
+    action: CSAction? = null
 ) = onMain { view.snackInfo(this, text, time, action) }
