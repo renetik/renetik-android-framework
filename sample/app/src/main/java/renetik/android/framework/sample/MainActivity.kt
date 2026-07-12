ad5c626beabe3923f9fd6f042dcfccfb1151d505
Rene Dohan <dohan.rene@gmail.com>
2026-07-12 16:58:52 +0800

    Update `MainActivity` to adjust status bar appearance based on dark mode
    - Use `WindowCompat` in `onCreate` to set `isAppearanceLightStatusBars` dynamically.
    - Toggle the status bar appearance based on whether the system is currently in dark mode.

--- sample/app/src/main/java/renetik/android/framework/sample/MainActivity.kt
+++ sample/app/src/main/java/renetik/android/framework/sample/MainActivity.kt
@@ -1,12 +1,16 @@
 package renetik.android.framework.sample
 
 import android.os.Bundle
+import androidx.core.view.WindowCompat
 import renetik.android.controller.base.CSViewActivity
+import renetik.android.core.android.content.res.isDarkMode
 
 class MainActivity : CSViewActivity<MainView>() {
     override fun createView() = MainView(this)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        WindowCompat.getInsetsController(window, window.decorView)
+            .isAppearanceLightStatusBars = !resources.configuration.isDarkMode
         setContentView()
     }
 }
